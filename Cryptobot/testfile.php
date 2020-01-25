dc5f953bc98b7e3c8284206a1aa5227163003f31
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-01-25 20:14:39 +0000

    test multiple new Reutrn Pattern

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -73,8 +73,10 @@ Echo "<BR> THIS IS A NEW TEST! ";
 //8368.73560680 BTC Price
 //2.192246 Charge
 
-echo "<BR> newBuywithPattern : ".newBuywithPattern('-1-1-11','-100-1,000-1,-1-1-11',1);
+//echo "<BR> newBuywithPattern : ".newBuywithPattern('-1-1-11','-100-1,000-1,-1-1-11',1);
 echo "<BR> newReturnPattern : ".newReturnPattern('1-1-11','1-1-11');
+echo "<BR> newReturnPattern : ".newReturnPattern('1-111','1-1-11');
+echo "<BR> newReturnPattern : ".newReturnPattern('10-11','1-1-11');
 
 ?>
 </html>
