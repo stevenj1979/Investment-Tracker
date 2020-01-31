1a5bb01ea1b6010fa1241f897b7613625da50c0d
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-01-31 15:56:50 +0000

    add Encrypt

---
+++ Encrypt.php
@@ -0,0 +1,10 @@
+<?php
+
+$key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
+
+$nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+$ciphertext = sodium_crypto_secretbox('This is a secret!', $nonce, $key);
+
+echo $ciphertext;
+
+?>
