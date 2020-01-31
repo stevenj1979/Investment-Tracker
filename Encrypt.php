f93c44d1b822e966aeae20ccf17e750b72a0f509
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-01-31 19:02:22 +0000

    update encrypt.php

--- Encrypt.php
+++ Encrypt.php
@@ -5,6 +5,6 @@ $key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
 $nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
 $ciphertext = sodium_crypto_secretbox('This is a secret!', $nonce, $key);
 
-echo $ciphertext;
+echo "<BR> This is a test: ".$ciphertext;
 
 ?>
