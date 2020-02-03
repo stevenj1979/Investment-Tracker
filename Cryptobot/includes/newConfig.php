0fd27a8071366025e881bb17dd571e22b6b205a5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-02-03 15:31:07 +0000

    add KEK to Britrex Reqs BuySellCoins

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -10,7 +10,7 @@ function getBittrexRequests(){
   }
 
   $sql = "SELECT `Type`,`BittrexRef`,`ActionDate`,`CompletionDate`,`Status`,`SellPrice`,`UserName`,`APIKey`,`APISecret`,`Symbol`,`Amount`,`CoinPrice`,`UserID`, `Email`,`OrderNo`,`TransactionID`,`BaseCurrency`,`RuleID`,`DaysOutstanding`,`timeSinceAction`,`CoinID`,
-  `RuleIDSell`,`LiveCoinPrice`,`TimetoCancelBuy`,`BuyOrderCancelTime` FROM `BittrexOutstandingRequests` WHERE `Status` = '1'";
+  `RuleIDSell`,`LiveCoinPrice`,`TimetoCancelBuy`,`BuyOrderCancelTime`,`KEK` FROM `BittrexOutstandingRequests` WHERE `Status` = '1'";
   $conn->query("SET time_zone = '+04:00';");
   $result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
@@ -18,7 +18,7 @@ function getBittrexRequests(){
   while ($row = mysqli_fetch_assoc($result)){
     $tempAry[] = Array($row['Type'],$row['BittrexRef'],$row['ActionDate'],$row['CompletionDate'],$row['Status'],$row['SellPrice'],$row['UserName'],$row['APIKey'],$row['APISecret'],$row['Symbol'],$row['Amount'],
     $row['CoinPrice'],$row['UserID'],$row['Email'],$row['OrderNo'],$row['TransactionID'],$row['BaseCurrency'],$row['RuleID'],$row['DaysOutstanding'],$row['timeSinceAction'],$row['CoinID'],$row['RuleIDSell'],$row['LiveCoinPrice'],
-    $row['TimetoCancelBuy'],$row['BuyOrderCancelTime']);
+    $row['TimetoCancelBuy'],$row['BuyOrderCancelTime'],$row['KEK']);
   }
   $conn->close();
   return $tempAry;
