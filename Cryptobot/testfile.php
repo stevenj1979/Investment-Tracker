fcfce221bb7f3578cdb9ea4e0d480f0257c41482
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-02-03 09:57:06 +0000

    test update

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -6,8 +6,8 @@ require('includes/newConfig.php');
 include_once ('/home/stevenj1979/SQLData.php');
 include_once ('/home/stevenj1979/Encrypt.php');
 
-$apikey=getAPIKey();
-$apisecret=getAPISecret();
+//$apikey=getAPIKey();
+//$apisecret=getAPISecret();
 
 $tmpTime = "+5 seconds";
 if (!empty($argv[1])){
