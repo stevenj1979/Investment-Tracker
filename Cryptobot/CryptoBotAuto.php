2b750d7de0936fd3e959cc8453fa499240f0ece5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-02-23 21:20:11 +0000

    sleep(60);

--- Cryptobot/CryptoBotAuto.php
+++ Cryptobot/CryptoBotAuto.php
@@ -131,7 +131,7 @@ while($date <= $newTime){
     //sleep(1);
   }//loop Coins
   echo "<br> SLEEP START: ".date("Y-m-d H:i:s", time());
-  sleep(45);
+  sleep(60);
   //wait(10000000);
   echo "<br> SLEEP END: ".date("Y-m-d H:i:s", time());
   $pauseStart = date("Y-m-d H:i", time());
