4fd83278d3568ddff3c3d200309d1a24113837fa
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-03-21 13:24:16 +0000

    add the balance

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -84,7 +84,8 @@ $current_date = date('Y-m-d H:i');
 //buyCoins('714f3f7873a2481c9f89b7c1f3801f2d', '2377fc19e47b4c7fb9dd32a70edd3b9f','BTC', 'stevenj1979@gmail.com', 3, '2020-03-21 12:54:14', 'USDT',1,1,0.10000000, 26,'stevenj1979',84,1.5,0,1,90,8);
 
 buyCoins('714f3f7873a2481c9f89b7c1f3801f2d', '2377fc19e47b4c7fb9dd32a70edd3b9f','BTC', 'stevenj1979@gmail.com', 3, '2020-03-21 16:16:41', 'USDT',1,1,0.00000000, 26,'stevenj1979',84,1.50,0,1,90,8);
-
+$BTCBalance = bittrexbalance('714f3f7873a2481c9f89b7c1f3801f2d', '2377fc19e47b4c7fb9dd32a70edd3b9f','USDT');
+echo "<BR> Balance : $BTCBalance ";
 //714f3f7873a2481c9f89b7c1f3801f2d, 2377fc19e47b4c7fb9dd32a70edd3b9f,'BTC', 'stevenj1979@gmail.com', 3, '2020-03-21 16:16:41', 'USDT',1,1,0.10000000, 26,'stevenj1979',84,1.50,0,1,90,8
 
 
