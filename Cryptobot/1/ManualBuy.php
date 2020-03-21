6df5431d74327a5caafdabac14f3ebd7d9fee3bd
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-03-21 14:19:48 +0000

    add fixed sell rule to manual buy

--- Cryptobot/1/ManualBuy.php
+++ Cryptobot/1/ManualBuy.php
@@ -122,7 +122,7 @@ if(isset($_POST['coinTxt'])){
   }
 
   buyCoins($_SESSION['APIKey'],$_SESSION['APISecret'],$_SESSION['coin'],$_SESSION['Email'],$_SESSION['ID'],$date,$_SESSION['baseCurrency']
-  ,1,1,$_SESSION['BTCBuyAmount'],99999,$_SESSION['UserName'],$_SESSION['coinID'],0,0,1,$_SESSION['TimeToCancelBuyMinsTxt']);
+  ,1,1,$_SESSION['BTCBuyAmount'],99999,$_SESSION['UserName'],$_SESSION['coinID'],0,0,1,$_SESSION['TimeToCancelBuyMinsTxt'],'ALL');
   //header('Location: BuyCoins.php');
 }
 
