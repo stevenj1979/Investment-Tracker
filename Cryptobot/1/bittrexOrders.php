8dca7f2908d585df62ac6643795ec532e20246b9
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-18 20:24:51 +0100

    remove sql echo

--- Cryptobot/1/bittrexOrders.php
+++ Cryptobot/1/bittrexOrders.php
@@ -64,7 +64,7 @@ function getBTTrackingCoins($userID){
 
   $sql = "SELECT `Type`,`BittrexRef`,`ActionDate`,`CompletionDate`,`Status`,`SellPrice`,`UserName`,`APIKey`,`APISecret`,`Symbol`,`Amount`,`CoinPrice`,`UserID`,`Email`,`OrderNo`,
   `TransactionID`,`BaseCurrency`,`LiveCoinPrice` FROM `BittrexOutstandingRequests` WHERE `userID` = $userID and $sqlOption order by `ActionDate` desc limit 50";
-  echo "<BR>$sql";
+  //echo "<BR>$sql";
   $result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
 //mysqli_fetch_assoc($result);
