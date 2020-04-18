6fe1c2dc7527e24d7339b16771c2f6f5c6c2cffc
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-18 21:30:55 +0100

    include_once '../includes/newConfig.php';

--- Cryptobot/1/ProfitPerMonth.php
+++ Cryptobot/1/ProfitPerMonth.php
@@ -5,7 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<?php require('includes/config.php');?>
+<?php require('includes/config.php');
+include_once '../includes/newConfig.php';?>
 <style>
 <?php include 'style/style.css'; ?>
 
