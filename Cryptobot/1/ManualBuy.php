b1fed20fc7baf49ed68f285a8d53c5fc49bf036c
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-18 14:10:11 +0100

    POST to GET

--- Cryptobot/1/ManualBuy.php
+++ Cryptobot/1/ManualBuy.php
@@ -89,7 +89,7 @@ if($_GET['alert'] <> ""){
   displaySideColumn();
   ?>&nbsp<?php
   displayMiddleColumn();
-  $GLOBALS['coin'] = $_POST['coinAlt']; $GLOBALS['cost'] = $_POST['coinPrice'];
+  $GLOBALS['coin'] = $_Get['coinAlt']; $GLOBALS['cost'] = $_GET['coinPrice'];
   displayAlertForm();
   displayFarSideColumn();
   ?>&nbsp<?php
