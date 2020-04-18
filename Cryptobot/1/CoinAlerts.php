56cb38026d3a47047347737f8c7b601248f4c9c7
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-18 20:38:45 +0100

    add Echo

--- Cryptobot/1/CoinAlerts.php
+++ Cryptobot/1/CoinAlerts.php
@@ -24,8 +24,8 @@ include_once ('/home/stevenj1979/SQLData.php');
 
         displayHeader(8);
 
-        Print_r("<h2>Buy Some Coins Now!</h2><Table><th>&nbspCoin</th><TH>&nbspBase Currency</th><TH>&nbspPrice</th><TH>&nbspMarket Cap %</th><TH>&nbspVolume by %</th><TH>&nbspBuy Orders %</th><TH>&nbspPrice Diff 1</th><TH>&nbspPrice Change</th><TH>&nbsp% Change 1Hr</th><TH>&nbsp% Change 24 Hrs</th>
-        <TH>&nbsp% Change 7 Days</th><TH>&nbspBuy Pattern</th><TH>&nbspManual Buy</th><TH>&nbspSet Alert</th><tr>");
+        echo "<h2>Buy Some Coins Now!</h2><Table><th>&nbspCoin</th><TH>&nbspBase Currency</th><TH>&nbspPrice</th><TH>&nbspMarket Cap %</th><TH>&nbspVolume by %</th><TH>&nbspBuy Orders %</th><TH>&nbspPrice Diff 1</th><TH>&nbspPrice Change</th><TH>&nbsp% Change 1Hr</th><TH>&nbsp% Change 24 Hrs</th>
+        <TH>&nbsp% Change 7 Days</th><TH>&nbspBuy Pattern</th><TH>&nbspManual Buy</th><TH>&nbspSet Alert</th><tr>";
         $newArrLength = 3;
 				for($x = 0; $x < $newArrLength; $x++) {
           echo "<BR> $x";
