c9c8469bb118d3d8a1b8c90f247fdfcc4ab46efe
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-19 16:56:57 +0100

    /home/stevenj1979/Investment-Tracker/Cryptobot/includes/newConfig.php

--- Cryptobot/1/m/BuyCoins.php
+++ Cryptobot/1/m/BuyCoins.php
@@ -4,7 +4,7 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
 <?php require('/home/stevenj1979/Investment-Tracker/Cryptobot/1/includes/config.php');
-include_once '/home/stevenj1979/Investment-Tracker/Cryptobot/1/includes/newConfig.php';?>
+include_once '/home/stevenj1979/Investment-Tracker/Cryptobot/includes/newConfig.php';?>
 <style>
 <?php include '/home/stevenj1979/Investment-Tracker/Cryptobot/1/m/style/style.css'; ?>
 </style> <?php
