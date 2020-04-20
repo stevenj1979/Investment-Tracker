73e9f70fe1971a7cc2fa0ad2bcbf32c53d9a8b5c
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-20 18:09:01 +0100

    remove isMobile

--- Cryptobot/1/login.php
+++ Cryptobot/1/login.php
@@ -44,9 +44,6 @@ function checkFirstTime($nameUser){
   return $tempAry;
 }
 
-function isMobile() {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-}
 //process login form if submitted
 if(isset($_POST['submit'])){
 
