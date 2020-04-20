0514921e8e9670be319027f1df502d36fff9942b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-20 09:04:22 +0100

    add iSMobile Function

--- Cryptobot/1/login.php
+++ Cryptobot/1/login.php
@@ -47,6 +47,9 @@ function checkFirstTime($nameUser){
   return $tempAry;
 }
 
+function isMobile() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
 //process login form if submitted
 if(isset($_POST['submit'])){
 
