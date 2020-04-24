a5c0d6221d17039c62e5a20f11887f7cffb603c5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-24 16:40:17 +0100

    add ?>

--- Cryptobot/1/BuySettings.php
+++ Cryptobot/1/BuySettings.php
@@ -241,7 +241,7 @@ displayHeader(7);?>
             }
           }
           echo "</table> <br><a href='AddNewSetting.php?addNew=Yes'><span class='glyphicon glyphicon-plus' style='font-size:48px;'></span></a>";
-          //echo "</table> <br><a href='AddNewSetting.php?addNew=Yes'>Add New</a>";
+          //echo "</table> <br><a href='AddNewSetting.php?addNew=Yes'>Add New</a>";?>
           <table>
             <th>&nbspEdit</th><th>&nbspCopy</th><th>&nbspDelete</th><TH>&nbspRuleID</TH><TH>&nbspUserID</TH><TH>&nbspBuyOrdersEnabled</TH><TH>&nbspBuyOrdersTop</TH><TH>&nbspBuyOrdersBtm</TH><TH>&nbspMarketCapEnabled</TH><TH>&nbspMarketCapTop</TH><TH>&nbspMarketCapBtm</TH>
             <TH>&nbsp1HrChangeEnabled</TH><TH>&nbsp1HrChangeTop</TH><TH>&nbsp1HrChangeBtm</TH><TH>&nbsp24HrChangeEnabled</TH><TH>&nbsp24HrChangeTop</TH><TH>&nbsp24HrChangeBtm</TH><TH>&nbsp7DChangeEnabled</TH><TH>&nbsp7DChangeTop</TH>
