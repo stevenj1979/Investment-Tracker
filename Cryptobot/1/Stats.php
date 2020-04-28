3edcef3e6832346db036a6a67d8c1b0b2e070502
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-28 12:50:10 +0100

    less than 15 mins

--- Cryptobot/1/Stats.php
+++ Cryptobot/1/Stats.php
@@ -85,7 +85,7 @@ function getHistoryFromSQL(){
     `ID`,`Symbol`,`LiveBuyOrders`,`LastBuyOrders`,`BuyOrdersPctChange`,`LiveMarketCap`,`LastMarketCap`,`MarketCapPctChange`,`Live1HrChange`,`Last1HrChange`,
     `Hr1ChangePctChange`,`Live24HrChange`,`Last24HrChange`,`Hr24ChangePctChange`,`Live7DChange`,`Last7DChange`,`D7ChangePctChange`,`LiveCoinPrice`,`LastCoinPrice`,
     `CoinPricePctChange`,`LiveSellOrders`,`LastSellOrders`,`SellOrdersPctChange`,`LiveVolume`,`LastVolume`,`VolumePctChange`,`BaseCurrency`,`ActionDate`
-    FROM `CoinBuyHistory` WHERE $sql_option and $sql_option_base and `ActionDate` >= curdate() - INTERVAL DAYOFWEEK(curdate())+2 HOUR
+    FROM `CoinBuyHistory` WHERE $sql_option and $sql_option_base and `ActionDate` >= curdate() - INTERVAL DAYOFWEEK(curdate())-15 MINUTE
     order by `ActionDate` desc";
     $result = $conn->query($sql);
     //echo $sql;
