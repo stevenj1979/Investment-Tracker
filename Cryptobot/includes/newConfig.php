5b31eba0f8af345ffb51323df889318bc4530a8f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-04-29 15:49:47 +0100

    remove echo

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -1644,9 +1644,9 @@ function setStats(){
 function getBase($selected, $statsAry){
     $statsOptionCount = Count($statsAry);
     for($x = 0; $x < $statsOptionCount; $x++) {
-      echo "<BR> If check : 1; ".$statsAry[$x][2]." : 2; ".$statsAry[1][$x]." : SELECTED; ".$selected;
+      //echo "<BR> If check : 1; ".$statsAry[$x][2]." : 2; ".$statsAry[1][$x]." : SELECTED; ".$selected;
       if ($statsAry[0][$x] == $selected){
-        echo "<BR> Base Currency = ".$statsAry[$x][2];
+        //echo "<BR> Base Currency = ".$statsAry[$x][2];
         return $statsAry[$x][2];
         exit;
       }
