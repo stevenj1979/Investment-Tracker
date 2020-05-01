c5ac8679e29a10cb23551fc25bc1203af8aad9d8
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-01 15:43:55 +0100

    $temp['data'][1]['quote'][1]['market_cap'];

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -519,7 +519,7 @@ function newCoinMarketCapStats(){
 
   $response = curl_exec($curl); // Send the request, save the response
   $temp = var_Dump(json_decode($response, true));
-  echo "<br>HERE! ".$temp['data'][1]['quote']['market_cap'];
+  echo "<br>HERE! ".$temp['data'][1]['quote'][1]['market_cap'];
   //$tmpCMCAry = $temp['Data']['Quote']['BTC']['market_cap'];
   //print_r(json_decode($response)); // print json decoded response
   curl_close($curl); // Close request
