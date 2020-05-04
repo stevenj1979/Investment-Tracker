4a1fe3df50e7b6c441bb020d7fe5114b6def3073
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-04 10:53:22 +0100

    remove -1

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -1789,7 +1789,7 @@ function NewEcho($textStr, $isMobile, $display){
 }
 
 function replaceStars($tempStr,$starCount){
-  for ($j = -1; $j<$starCount-1; $j++){
+  for ($j = -1; $j<$starCount; $j++){
     for ($k = -1; $k<3-1; $k++){
       echo "<BR> $j $k";
     }
