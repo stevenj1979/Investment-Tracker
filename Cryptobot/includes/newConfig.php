30bce0e23040d2f286a2a8dbfa4a070848f369d1
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-04 11:35:13 +0100

    remove wildcard test

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -1856,17 +1856,8 @@ Function removeWildcard($wildcardStr){
         $starCount =substr_count($tempStr[$i],"*");
         //$returntempStr = $tempStr[$i].",";
         for ($x=0; $x<$starCount; $x++){
-          if ($x == 0){
-            //Replace all instances of * with 1 EG *-1-11 > 1-1-11
-            //$returntempStr .=str_replace("*","1",$tempStr[$i],1).",";
-            //Replace all instances of * with 0 EG *-1-11 > 0-1-11
-            //$returntempStr .=str_replace("*","0",$tempStr[$i],1).",";
-            //Replace all instances of * with -1 EG *-1-11 > -1-1-11
-            //$returntempStr .=str_replace("*","-1",$tempStr[$i],1).",";
-            //add the 3 srings to the return str
-          }else{
-
-          }
+          $newStr = returnWildcardStr($tempStr[$i],$starCount);
+          $returntempStr = replaceStars($newStr,$starCount);
         }
         $returnStr .= $returntempStr;
     }else{
