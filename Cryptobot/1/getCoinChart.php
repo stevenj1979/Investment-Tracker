4990e2dc47c305d1243b02c387b1b74391bcfa03
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-06 09:58:51 +0100

    set timezone on coinchart

--- Cryptobot/1/getCoinChart.php
+++ Cryptobot/1/getCoinChart.php
@@ -8,7 +8,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $coinID = $_GET['coinID'];
-date_default_timezone_set('Asia/Dubai');
+setTimeZone();
 $time = str_replace("_"," ",$_GET['time']);
 //$query = "set time_zone='+04:00';";
 $query = "SELECT `ActionDate`,`LiveCoinPrice` as LiveCoinPrice
