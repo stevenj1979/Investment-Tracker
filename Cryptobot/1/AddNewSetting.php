3489dbe2f1d6ba7b161ef0f56cc7174ea4ef4679
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-12 14:04:42 +0100

    == to =

--- Cryptobot/1/AddNewSetting.php
+++ Cryptobot/1/AddNewSetting.php
@@ -35,10 +35,10 @@ if(!empty($_GET['editedUserReady'])){
       Echo " ".$_POST['publishHr1'].$_POST['selectCmbo1Hr1'].$_POST['selectCmbo1Hr2'].$_POST['selectCmbo1Hr3'].$_POST['selectCmbo1Hr4'];
   }elseif (!empty($_POST['publishTrend'])){
       Echo " ".$_POST['publishTrend'].$_POST['selectCmboTrend1'].$_POST['selectCmboTrend2'].$_POST['selectCmboTrend3'].$_POST['selectCmboTrend4'];
-      if ($_POST['selectCmboTrend1'] == 2){$temp1 = '*';$temp2 ==$_POST['selectCmboTrend2']; $temp3 == $_POST['selectCmboTrend3'];$temp4 == $_POST['selectCmboTrend4'];}
-      elseif ($_POST['selectCmboTrend2'] == 2){$temp1 == $_POST['selectCmboTrend1'];$temp2 = '*';$temp3 == $_POST['selectCmboTrend3'];$temp4 == $_POST['selectCmboTrend4'];}
-      elseif ($_POST['selectCmboTrend3'] == 2){$temp1 == $_POST['selectCmboTrend1'];$temp2 ==$_POST['selectCmboTrend2'];$temp3 = '*';$temp4 == $_POST['selectCmboTrend4'];}
-      elseif ($_POST['selectCmboTrend4'] == 2){$temp1 == $_POST['selectCmboTrend1'];$temp2 ==$_POST['selectCmboTrend2'];$temp3 == $_POST['selectCmboTrend3'];$temp4 = '*';}
+      if ($_POST['selectCmboTrend1'] == 2){$temp1 = '*';$temp2 = $_POST['selectCmboTrend2']; $temp3 = $_POST['selectCmboTrend3'];$temp4 = $_POST['selectCmboTrend4'];}
+      elseif ($_POST['selectCmboTrend2'] == 2){$temp1 = $_POST['selectCmboTrend1'];$temp2 = '*';$temp3 = $_POST['selectCmboTrend3'];$temp4 = $_POST['selectCmboTrend4'];}
+      elseif ($_POST['selectCmboTrend3'] == 2){$temp1 = $_POST['selectCmboTrend1'];$temp2 = $_POST['selectCmboTrend2'];$temp3 = '*';$temp4 = $_POST['selectCmboTrend4'];}
+      elseif ($_POST['selectCmboTrend4'] == 2){$temp1 = $_POST['selectCmboTrend1'];$temp2 = $_POST['selectCmboTrend2'];$temp3 = $_POST['selectCmboTrend3'];$temp4 = '*';}
       Echo "$temp1 $temp2 $temp3 $temp4 ";
   }else{
     //if (!empty($_POST['MarketCapEnable'])){if ($_POST['MarketCapEnable']== "Yes"){ $mCapEnChk = 1;}else{$mCapEnChk = 00;}}
