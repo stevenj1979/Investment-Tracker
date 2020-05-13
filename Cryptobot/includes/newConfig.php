acb22bd6db451e4906c636bbcabc41d56114cb4c
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-13 15:36:26 +0100

    more echos

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -1864,11 +1864,13 @@ function returnWildcardStr($tempStr, $starCount){
 }
 
 function stringsToArray($str1, $str2, $str3, $str4){
+  echo "<BR> STR1 ".$str1;
   $tmpAry1 = explode($str1,','); $tmpAry2 = explode($str2,',');
   $tmpAry3 = explode($str3,','); $tmpAry4 = explode($str4,',');
   $aryCount = count($tmpAry1); $returnAry = [];
+  echo "<BR> Count ".$aryCount;
   for ($i=0; $i<$aryCount; $i++){
-    echo "<BR> B :".$tmpAry1[$i][0];
+    echo "<BR> B :".$tmpAry1[$i];
     $returnAry[$i][0] = $tmpAry1[$i];
     $returnAry[$i][1] = $tmpAry2[$i];
     $returnAry[$i][2] = $tmpAry3[$i];
