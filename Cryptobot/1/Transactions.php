531b85e2d9e7537e8ba51193dd1059d7fcc89e15
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-13 11:46:45 +0100

    date format

--- Cryptobot/1/Transactions.php
+++ Cryptobot/1/Transactions.php
@@ -121,7 +121,7 @@ function getCoinsfromSQL($userID){
 
 function displayDefault(){
   $coin = getCoinsfromSQL($_SESSION['ID']);
-  if ($_SESSION['isMobile']){ $num = 2; $fontSize = "font-size:60px"; }else{$num = 8;$fontSize = "font-size:32px"; }
+  if ($_SESSION['isMobile']){ $num = 2; $fontSize = "font-size:60px"; $dformat ="YYYY/mm/dd" }else{$num = 8;$fontSize = "font-size:32px"; $dformat ="YYYY/mm/dd H:i:s"}
   $arrlength = count($coin);
   echo "<html><h2>Transactions</h2>";
   echo "<form action='Transactions.php?dropdown=Yes' method='post'>";
@@ -140,8 +140,8 @@ function displayDefault(){
   print_r("<th>Change Fixed Sell Rule</th>");
   print_r("<tr>");
   for($x = 0; $x < $arrlength; $x++) {
-      $Id = $coin[$x][0]; $coinPrice = round($coin[$x][3],$num); $amount  = round($coin[$x][4],$num); $status  = $coin[$x][5]; $orderDate = $coin[$x][6]; $bittrexRef = $coin[$x][9];
-      $orderNo = $coin[$x][14];$symbol = $coin[$x][15]; $fixSellRule = $coin[$x][16];
+      $Id = $coin[$x][0]; $coinPrice = round($coin[$x][3],$num); $amount  = round($coin[$x][4],$num); $status  = $coin[$x][5]; $orderDate = date_format($coin[$x][6],$dformat);
+      $bittrexRef = $coin[$x][9];$orderNo = $coin[$x][14];$symbol = $coin[$x][15]; $fixSellRule = $coin[$x][16];
       $purchasePrice = round($amount*$coinPrice,$num);
       print_r("<td>$Id</td>");
       NewEcho("<td>$orderNo</td>",$_SESSION['isMobile'],0);
