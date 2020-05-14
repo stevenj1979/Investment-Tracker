3eaf65e31043d2832fa164fe76914247b6f02107
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-14 11:54:18 +0100

    echo trans select

--- Cryptobot/1/Transactions.php
+++ Cryptobot/1/Transactions.php
@@ -51,7 +51,7 @@ if($_POST['transSelect'] <> ""){
 function changeSelection(){
   //global $sql_option;
   //global $dropArray;
-  //echo "<BR> TransSelect : ".$_POST['transSelect'];
+  echo "<BR> TransSelect : ".$_POST['transSelect'];
   if ($_POST['transSelect']=='Open'){
      $_SESSION['TransListSelected'] = "Open";
      //$dropArray[] = Array("Open","Sold","All");
@@ -66,6 +66,7 @@ function changeSelection(){
     //$dropArray[] = Array("All","Open","Sold");
   }
   //print_r($globals['sql_Option']);
+  echo "<BR> TransSelect AFTER : ".$_POST['transSelect'];
 }
 
 function displayChangeFix($fixSellRule, $transID){
