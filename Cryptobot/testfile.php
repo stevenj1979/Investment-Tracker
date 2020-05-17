23ac485d090591522f539be5360671c88a07c0d9
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-17 10:46:42 +0100

    test file

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -67,7 +67,7 @@ function SQLCommand(){
 }
 
 $autoBuyPrice = getAutoBuyPrices();
-echo "<BR> TEST: ".autoBuyMain(9497,$autoBuyPrice,1,84);
+echo "<BR> TEST: ".autoBuyMain(8697,$autoBuyPrice,1,84);
 
 echo "<BR> TEST: ".autoBuy(9497,9570,6190,1);
 
