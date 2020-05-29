ced6260f5ce6b7f802d3ae2c4784bdabde5deea2
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-05-29 14:17:06 +0100

    <BR>

--- Cryptobot/1/Settings.php
+++ Cryptobot/1/Settings.php
@@ -178,7 +178,8 @@ $userDetails = getUserIDs($_SESSION['ID']);
                         echo "<option value='".$option1."'>".$option1."</option>
                         <option value='".$option2."'>".$option2."</option>
                         <option value='".$option3."'>".$option3."</option>
-                        <option value='".$option4."'>".$option4."</option></select></div>";
+                        <option value='".$option4."'>".$option4."</option></select></div><BR>";
+                        
                         Echo "<select name='$name' size='3'>";
                         for ($i=0; $i<$sequenceCount; $i++){
                           $sellRuleID = $sequence[$i][0]; $newSeq = $sequence[$i][1];
