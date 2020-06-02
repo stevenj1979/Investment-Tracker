5b635793145608c56ed7bc497139812b9e57fd5f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-02 21:45:51 +0100

    missing .

--- Cryptobot/CryptoBotAuto.php
+++ Cryptobot/CryptoBotAuto.php
@@ -95,7 +95,7 @@ while($date <= $newTime){
     $bitPrice = number_format((float)(bittrexCoinPrice($apikey,$apisecret,$baseCurrency,$symbol)), 8, '.', '');
     echo "<br> PRICE_UPDATE COIN= $symbol CoinPrice= $bitPrice time ".date("Y-m-d H:i", time());
     $lastUpdateTime = $timeAry[$coinID];
-    echo "<BR> TimeTest $coinID : $lastUpdateTime : $secondstoUpdate : "date("Y-m-d H:i", time())." : ".timerReady($lastUpdateTime,$secondstoUpdate);
+    echo "<BR> TimeTest $coinID : $lastUpdateTime : $secondstoUpdate : ".date("Y-m-d H:i", time())." : ".timerReady($lastUpdateTime,$secondstoUpdate);
     if (timerReady($lastUpdateTime,$secondstoUpdate)){
       copyCoinPrice($coinID,$bitPrice);
       $timeAry[$coinID] = date("Y-m-d H:i", time());
