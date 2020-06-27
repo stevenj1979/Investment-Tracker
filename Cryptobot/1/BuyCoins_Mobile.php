883310a893464ac13ab1392e773b2e737712bc7b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-27 14:17:30 +0100

    test colour

--- Cryptobot/1/BuyCoins_Mobile.php
+++ Cryptobot/1/BuyCoins_Mobile.php
@@ -194,6 +194,7 @@ displayHeader(3);
           NewEcho("<td id='tCoin'>$coin</td>",$_SESSION['isMobile'],1);
           $priceChange = round($priceChange,2);
           $numCol = getNumberColour($priceChange);
+          echo "Test Colour: $numCol";
           NewEcho("<td id='tPriceChng' color=$numCol>$priceChange</td>",$_SESSION['isMobile'],1);
           NewEcho("<td id='cNicon' rowspan='2'><a href='ManualBuy.php?coin=$coin&baseCurrency=$baseCurrency&coinID=$coinID&coinPrice=$bitPrice'><i class='fas fa-shopping-cart' style='$fontSize;color:#D4EFDF'></i></a></td>",$_SESSION['isMobile'],1);
           NewEcho("<td id='cNicon' rowspan='2'><a href='CoinAlerts.php?alert=0&coinAlt=$coin&baseCurrency=$baseCurrency&coinID=$coinID&coinPrice=$bitPrice'><i class='fas fa-bell' style='$fontSize;color:#D4EFDF'></i></a></td>",$_SESSION['isMobile'],1);
