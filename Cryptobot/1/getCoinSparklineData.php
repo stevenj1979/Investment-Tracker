b46bab509e573c6235d3795c90d77a8786cf4f3b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-27 15:36:17 +0100

    only an array in Sparklines Data

--- Cryptobot/1/getCoinSparklineData.php
+++ Cryptobot/1/getCoinSparklineData.php
@@ -25,36 +25,16 @@ $query = "SELECT `LiveCoinPrice` as LiveCoinPrice
   FROM `CoinBuyHistory`
   WHERE  (`ActionDate` > DATE_SUB((select Max(`ActionDate`) from `CoinBuyHistory`), INTERVAL 1 Hour)) and `ID` = (select Max(`ID`) from `Coin` where `Symbol` = 'BTC')
   order by `ActionDate` asc ";
-$table = array();
-$table['cols'] = array(
-    /* define your DataTable columns here
-     * each column gets its own array
-     * syntax of the arrays is:
-     * label => column label
-     * type => data type of column (string, number, date, datetime, boolean)
-     */
-    // I assumed your first column is a "string" type
-    // and your second column is a "number" type
-    // but you can change them if they are not
-    array('label' => 'LiveCoinPrice', 'type' => 'number')
-);
 
-$rows = array();
 $result = $conn->query($query);
+$tempAry[] = Array('LiveCoinPrice');
 while ($row = mysqli_fetch_assoc($result)){
-    $temp = array();
-    // each column needs to have data inserted via the $temp array
-    //$temp[] = array('v' => (float) $row['TotalBTC']*$btcPrice);
-    $temp[] = array('v' => (float) $row['LiveCoinPrice']);
-    // insert the temp array into $rows
-    $rows[] = array('c' => $temp);
+  $tempAry[] = Array($row['LiveCoinPrice']);
 }
 
-// populate the table with rows of data
-$table['rows'] = $rows;
 
 // encode the table as JSON
-$jsonTable = json_encode($table);
+$jsonTable = json_encode($tempAry);
 
 // set up header; first two prevent IE from caching queries
 header('Cache-Control: no-cache, must-revalidate');
