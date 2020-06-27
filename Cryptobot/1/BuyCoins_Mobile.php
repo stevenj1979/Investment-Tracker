4db92ea4a143b1ab5f8bbb3558dd4908c2d1c3da
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-27 13:22:24 +0100

    remove rowspan

--- Cryptobot/1/BuyCoins_Mobile.php
+++ Cryptobot/1/BuyCoins_Mobile.php
@@ -186,7 +186,7 @@ displayHeader(3);
         //Table
           NewEcho("<table border=1><tr>",$_SESSION['isMobile'],1);
           NewEcho("<td>CoinName</td>",$_SESSION['isMobile'],1);
-          NewEcho( "<td rowspan=2>Chart</td>",$_SESSION['isMobile'],1);
+          NewEcho( "<td>Chart</td>",$_SESSION['isMobile'],1);
           NewEcho( "<td>$bitPrice</td>",$_SESSION['isMobile'],1,4);
           echo "</tr><tr>";
           NewEcho("<td>$coin</td>",$_SESSION['isMobile'],1);
