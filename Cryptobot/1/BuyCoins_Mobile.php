f086c0c4c37a30fbf7ec53e6ca877a22a3906a29
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-27 13:19:01 +0100

    add border

--- Cryptobot/1/BuyCoins_Mobile.php
+++ Cryptobot/1/BuyCoins_Mobile.php
@@ -184,13 +184,13 @@ displayHeader(3);
         $Hr1LivePriceChange = $tracking[$x][31];$Hr1LastPriceChange = $tracking[$x][32]; $Hr1PriceChange3 = $tracking[$x][33];$Hr1PriceChange4 = $tracking[$x][34];
         $new1HrPriceChange = $Hr1PriceChange4.$Hr1PriceChange3.$Hr1LastPriceChange.$Hr1LivePriceChange;
         //Table
-          NewEcho("<table>",$_SESSION['isMobile'],1);
+          NewEcho("<table border=1>",$_SESSION['isMobile'],1);
           NewEcho("<td>CoinName</td>",$_SESSION['isMobile'],1);
           NewEcho( "<td rowspan=2>Chart</td>",$_SESSION['isMobile'],1);
-          NewEcho( "<td>$bitPrice</td>",$_SESSION['isMobile'],1,2);
+          NewEcho( "<td>$bitPrice</td>",$_SESSION['isMobile'],1,4);
           echo "<tr>";
           NewEcho("<td>$coin</td>",$_SESSION['isMobile'],1);
-          NewEcho("<td>$priceChange</td>",$_SESSION['isMobile'],1,2);
+          NewEcho("<td>$priceChange</td>",$_SESSION['isMobile'],1,3);
 
         echo "<tr>";
       }//end for
