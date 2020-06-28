2c511cfb03a06008f84d4b4dea18623867bf1459
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-06-28 19:00:30 +0100

    create images for all Coins

--- Cryptobot/AutoUpdatePrice.php
+++ Cryptobot/AutoUpdatePrice.php
@@ -223,12 +223,17 @@ for($x = 0; $x < $finalMergeArySize; $x++) {
 
 echo "<BR> Generate sparkline Images";
 //$sparklineAry = [];
-$sparklineAry = getSparklineData('BTC');
-$url ="http://www.investment-tracker.net/Sparkline/sparkline.php?size=80x20&data=";
-$url2 = "&back=fff&line=5bb763&fill=d5f7d8";
-$data = dataToString(",",$sparklineAry);
-$savePath ="/home/stevenj1979/public_html/Investment-Tracker/Cryptobot/";
-saveImage('BTC',$url.$data.$url2,$savePath);
+$trackingCoins = getNewTrackingCoins();
+$coinSize = Count($trackingCoins);
+for ($j=0; $j<$coinSize; $j++){
+  $sparklineAry = getSparklineData($trackingCoins[$j][3]);
+  $url ="http://www.investment-tracker.net/Sparkline/sparkline.php?size=80x20&data=";
+  $url2 = "&back=fff&line=5bb763&fill=d5f7d8";
+  $data = dataToString(",",$sparklineAry);
+  $savePath ="/home/stevenj1979/public_html/Investment-Tracker/Cryptobot/Images/";
+  saveImage('BTC',$url.$data.$url2,$savePath);
+}
+
 //$sparkline = new Davaxi\Sparkline();
 //$sparkline->setData(array(2,4,5,6,10,7,8,5,7,7,11,8,6,9,11,9,13,14,12,16));
 //$sparkline->save('/home/stevenj1979/repositories/Sparkline/BTC');
