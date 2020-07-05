e4647ee726567bf14fc3260f3bfe4da18fe4149e
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-05 14:42:56 +0100

    max execution time 900

--- Cryptobot/AutoUpdatePrice.php
+++ Cryptobot/AutoUpdatePrice.php
@@ -1,6 +1,6 @@
 <html>
 <?php
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 900);
 require('includes/newConfig.php');
 //require '/home/stevenj1979/repositories/Sparkline/autoload.php';
 include_once ('/home/stevenj1979/SQLData.php');
