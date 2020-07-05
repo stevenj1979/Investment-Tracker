b063df1279589da5224fab7ef82119ecc4be961f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-05 14:01:44 +0100

    fix adding new array entry

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -2246,17 +2246,27 @@ function updateMergeAry($toMergeAry, $finalMergeAry){
   for ($j=0; $j<$finalMergeArySize; $j++){
     if ($toMergeAry[0] == $finalMergeAry[$j][0] && $toMergeAry[1] == $finalMergeAry[$j][1]){
       //User/Coin exist
+      echo "<BR> EXISTING is TRUE ";
       $existing = True;
       $finalMergeAry[$j][4] = $finalMergeAry[$j][4]+$toMergeAry[4];
+      echo "<BR> adding ".$finalMergeAry[$j][4]."+".$toMergeAry[4];
       $finalMergeAry[$j][5] = $finalMergeAry[$j][5]+$toMergeAry[5];
+      echo "<BR> adding ".$finalMergeAry[$j][5]."+".$toMergeAry[5];
       $finalMergeAry[$j][6] = $toMergeAry[3];
+      echo "<BR> adding ".$toMergeAry[5];
       $finalMergeAry[$j][7] = $finalMergeAry[$j][7]+1;
+      echo "<BR> adding ".$finalMergeAry[$j][7]."+1";
     }
   }
   if ($existing == False){
-    $finalMergeAry[$finalMergeArySize+1][0] = $toMergeAry[0];
-    $finalMergeAry[$finalMergeArySize+1][1] = $toMergeAry[1];
-    $finalMergeAry[$finalMergeArySize+1][2] = $toMergeAry[2];
+    echo "<BR> EXISTING is FALSE";
+    if ($finalMergeArySize == 0) {$finalMergeArySize = $finalMergeArySize;} else {$finalMergeArySize = $finalMergeArySize+1;}
+    $finalMergeAry[$finalMergeArySize][0] = $toMergeAry[0];
+    echo "<BR> SETTING: ".$toMergeAry[0];
+    $finalMergeAry[$finalMergeArySize][1] = $toMergeAry[1];
+    echo "<BR> SETTING: ".$toMergeAry[1];
+    $finalMergeAry[$finalMergeArySize][2] = $toMergeAry[2];
+    echo "<BR> SETTING: ".$toMergeAry[2];
     $finalMergeAry[$finalMergeArySize+1][3] = $toMergeAry[3];
     $finalMergeAry[$finalMergeArySize+1][4] = $toMergeAry[4];
     $finalMergeAry[$finalMergeArySize+1][5] = $toMergeAry[5];
