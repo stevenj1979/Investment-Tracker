8740c19b2548ad5bb6c34c65750658e23acdacc1
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-20 13:28:25 +0100

    add {

--- Cryptobot/BuySellCoins.php
+++ Cryptobot/BuySellCoins.php
@@ -52,7 +52,7 @@ function getOutStandingSell(){
 function initiateAry($ary, $userID){
   if (array_key_exists($userID,$ary)){
       echo "<BR> Key Exists";
-  }else
+  }else{
       echo "<BR> Set Key to 0";
     $ary[$userID] = 0;
   }
