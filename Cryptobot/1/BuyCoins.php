9cf460dce21e99004dc050927865d36d7f997d2f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-21 12:47:10 +0100

    add echo

--- Cryptobot/1/BuyCoins.php
+++ Cryptobot/1/BuyCoins.php
@@ -27,6 +27,8 @@ if(isset($_GET['noOverride'])){
   if ($_SESSION['MobOverride'] == True){$_SESSION['MobOverride'] = False;}
 }
 
+Echo "<BR> isMobile: ".$_SESSION['isMobile']." | MobOverride: ".$_SESSION['MobOverride'];
+
 if ($_SESSION['isMobile'] && $_SESSION['MobOverride'] = False){
   header('Location: BuyCoins_Mobile.php');
 }
