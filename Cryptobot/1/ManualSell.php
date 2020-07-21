da405908722e7bf72a064c874fbd8d3038af1ab8
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-21 10:11:38 +0100

    set Transaction Pending

--- Cryptobot/1/ManualSell.php
+++ Cryptobot/1/ManualSell.php
@@ -28,6 +28,7 @@ if(isset($_GET['merge'])){
   $salePrice = trim($_GET['salePrice']);
   $userID = trim($_GET['userID']);
   newTrackingSellCoins($salePrice, $userID,$transactionID,1, 1,0,0);
+  setTransactionPending($transactionID);
   header('Location: SellCoins.php');
 }
 
