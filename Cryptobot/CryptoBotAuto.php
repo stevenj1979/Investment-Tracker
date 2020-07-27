d02198105a13066d3922138722a0a07d09f410da
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-27 19:04:49 +0100

    market cap update 360

--- Cryptobot/CryptoBotAuto.php
+++ Cryptobot/CryptoBotAuto.php
@@ -179,7 +179,7 @@ while($date <= $newTime){
   //if ($i >= 2){$historyFlag = False; $marketCapFlag = Flase;}
   $date = date("Y-m-d H:i", time());
   if (timerReady($history_date,120)){$historyFlag=True; $history_date = date('Y-m-d H:i'); Echo "<BR> History Timer ";logAction('Update History Set','CoinPrice', $logToFileSetting);}
-  if (timerReady($marketCap_date,550)){$marketCapFlag=True; $marketCap_date = date('Y-m-d H:i'); $marketCapStatsUpdateFlag = True; Echo "<BR> Market Cap Timer "; logAction('Market Cap Update Set','CoinPrice', $logToFileSetting);}
+  if (timerReady($marketCap_date,360)){$marketCapFlag=True; $marketCap_date = date('Y-m-d H:i'); $marketCapStatsUpdateFlag = True; Echo "<BR> Market Cap Timer "; logAction('Market Cap Update Set','CoinPrice', $logToFileSetting);}
 
 }//while loop
 echo "EndTime ".date("Y-m-d H:i", time());
