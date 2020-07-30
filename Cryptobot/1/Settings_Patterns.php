0e954a8ab4cf2a36e941a9a35da958fc1d9bacdb
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-30 18:36:19 +0100

    change to 2 options

--- Cryptobot/1/Settings_Patterns.php
+++ Cryptobot/1/Settings_Patterns.php
@@ -27,19 +27,19 @@ $title = 'CryptoBot';
 require('layout/header.php');
 
 
-if (!empty($_POST['CoinPriceMatchNamesSelect'])){
+if (!empty($_POST['CoinPriceMatchNamesSelect']) && !empty($_GET['changeNameSelection'])){
     //echo "<BR> coin price Match Names is ".$_POST['CoinPriceMatchNamesSelect'];
     //echo "<BR>  ID is ".$_POST['CoinPriceMatchNamesSelect'];
     setNameSelection($_POST['CoinPriceMatchNamesSelect']);
 }
 
-if (!empty($_POST['CoinPricePatternNamesSelect'])){
+if (!empty($_POST['CoinPricePatternNamesSelect']) && !empty($_GET['changeNameSelection'])){
     //echo "<BR> coin price Match Names is ".$_POST['CoinPriceMatchNamesSelect'];
     //echo "<BR>  ID is ".$_POST['CoinPriceMatchNamesSelect'];
     setNameSelectionPricePattern($_POST['CoinPricePatternNamesSelect']);
 }
 
-if (!empty($_POST['addPricePatternBtn'])){
+if (!empty($_POST['addPricePatternBtn']) && !empty($_GET['addPricePattern'])){
   $cmbo1 = $_POST['selectCmbo1Hr1'];$cmbo2 = $_POST['selectCmbo1Hr2'];
   $cmbo3 = $_POST['selectCmbo1Hr3']; $cmbo4 = $_POST['selectCmbo1Hr4'];
   $pattern = str_replace("2","*",$cmbo1.$cmbo2.$cmbo3.$cmbo4);
@@ -47,7 +47,7 @@ if (!empty($_POST['addPricePatternBtn'])){
   addTrendPatterntoSQL($pattern);
 }
 
-if (!empty($_POST['removePricePatternBtn'])){
+if (!empty($_POST['removePricePatternBtn']) && !empty($_GET['addPricePattern'])){
   $ID = $_POST['CoinPricePatternSelect'];
   //echo "<BR> Test REmove ID $ID";
   removePricePatternfromSQL($ID);
@@ -270,62 +270,62 @@ $comboList = Array('-1','0','1','*');
     echo "<input type='submit' name='newNameBtn' value='Add New Name'>";
   echo "</form>";
   echo "<form action='Settings_Patterns.php?addPrice=Y' method='post'>";
-  echo "<select name='CoinPriceMatchSelect' size='8'>";
-  for ($l=0; $l<$coinPriceMatchSize; $l++){
-      $name = $coinPriceMatch[$l][4]; $price = $coinPriceMatch[$l][1];
-      $lowPrice = $coinPriceMatch[$l][3]; $symbol = $coinPriceMatch[$l][2]; $coinID = $coinPriceMatch[$l][0];
-      $ID = $coinPriceMatch[$l][6]; $coinMatchNameID = $coinPriceMatch[$l][7]; $userID = $coinPriceMatch[$l][5];
-      echo "<option value='$ID+$coinMatchNameID+$userID'>$symbol | $price | $lowPrice</option>";
-  }
-  echo "</select>";
-  echo "<select name='symbol'>";
-  for ($m = 0; $m<$coinsSize; $m++){
-    $symbol = $coins[$m][0]; $coinID = $coins[$m][1];
-    echo "<option value='$coinID'>$symbol</option>";
-    //echo "<input type='text' name='symbol' id='symbol' class='form-control input-lg' placeholder='BTC' value='' tabindex='1'>";
-  }
-  echo "</select>";
-  echo "<input type='text' name='topPrice' id='topPrice' class='form-control input-lg' placeholder='8000.00' value='' tabindex='2'>";
-  echo "<input type='text' name='bttmPrice' id='bttmPrice' class='form-control input-lg' placeholder='0.00' value='' tabindex='3'>";
-  echo "<input type='submit' name='addPriceBtn' value='+'>";
-  echo "<input type='submit' name='removePriceBtn' value='-'>";
+    echo "<select name='CoinPriceMatchSelect' size='8'>";
+    for ($l=0; $l<$coinPriceMatchSize; $l++){
+        $name = $coinPriceMatch[$l][4]; $price = $coinPriceMatch[$l][1];
+        $lowPrice = $coinPriceMatch[$l][3]; $symbol = $coinPriceMatch[$l][2]; $coinID = $coinPriceMatch[$l][0];
+        $ID = $coinPriceMatch[$l][6]; $coinMatchNameID = $coinPriceMatch[$l][7]; $userID = $coinPriceMatch[$l][5];
+        echo "<option value='$ID+$coinMatchNameID+$userID'>$symbol | $price | $lowPrice</option>";
+    }
+    echo "</select>";
+    echo "<select name='symbol'>";
+    for ($m = 0; $m<$coinsSize; $m++){
+      $symbol = $coins[$m][0]; $coinID = $coins[$m][1];
+      echo "<option value='$coinID'>$symbol</option>";
+      //echo "<input type='text' name='symbol' id='symbol' class='form-control input-lg' placeholder='BTC' value='' tabindex='1'>";
+    }
+    echo "</select>";
+    echo "<input type='text' name='topPrice' id='topPrice' class='form-control input-lg' placeholder='8000.00' value='' tabindex='2'>";
+    echo "<input type='text' name='bttmPrice' id='bttmPrice' class='form-control input-lg' placeholder='0.00' value='' tabindex='3'>";
+    echo "<input type='submit' name='addPriceBtn' value='+'>";
+    echo "<input type='submit' name='removePriceBtn' value='-'>";
   echo "</form></div>";
 
 
   echo "<H3>Coin Price Pattern</H3>";
   echo "<div><form action='Settings_Patterns.php?changeNameSelection=Y' method='post'>";
-  Echo "<select name='CoinPricePatternNamesSelect'>";
-  for ($i=0; $i<$coinPricePatternNamesSize; $i++){
-    $name = $coinPricePatternNames[$i][0]; $nameID = $coinPricePatternNames[$i][1];
-    //$coinID = $coinPriceMatch[$i][4];$price = $coinPriceMatch[$i][2];
-    //$symbol = $coinPriceMatch[$i][3];$lowPrice = $coinPriceMatch[$i][1];
-    Echo "<BR> $name | $coinPricePatternNameSelected";
-    if ($nameID == $coinPricePatternNameSelected){
-      echo "<option value='$nameID' selected>$name</option>";
-    }else{
-      echo "<option value='$nameID'>$name</option>";
+    Echo "<select name='CoinPricePatternNamesSelect'>";
+    for ($i=0; $i<$coinPricePatternNamesSize; $i++){
+      $name = $coinPricePatternNames[$i][0]; $nameID = $coinPricePatternNames[$i][1];
+      //$coinID = $coinPriceMatch[$i][4];$price = $coinPriceMatch[$i][2];
+      //$symbol = $coinPriceMatch[$i][3];$lowPrice = $coinPriceMatch[$i][1];
+      Echo "<BR> $name | $coinPricePatternNameSelected";
+      if ($nameID == $coinPricePatternNameSelected){
+        echo "<option value='$nameID' selected>$name</option>";
+      }else{
+        echo "<option value='$nameID'>$name</option>";
+      }
     }
-  }
-  echo "</select>";
-  echo "<input type='submit' name='publishTrendPricePattern' value='Refresh'></form>";
+    echo "</select>";
+    echo "<input type='submit' name='publishTrendPricePattern' value='Refresh'></form>";
   echo "<form action='Settings_Patterns.php?addNewPricePatternName=Y' method='post'>";
     echo "<input type='text' name='newNamePricePatterntxt' id='newNamePricePatterntxt' class='form-control input-lg' placeholder='Name' value='' tabindex='1'>";
     echo "<input type='submit' name='newNamePricePatternBtn' value='Add New Name'>";
   echo "</form>";
   echo "<form action='Settings_Patterns.php?addPricePattern=Y' method='post'>";
-  echo "<select name='CoinPricePatternSelect' size='8'>";
-  for ($j=0; $j<$coinPricePatternSize; $j++){
-    $name = $coinPricePattern[$j][0]; $pattern = $coinPricePattern[$j][1];
-    $nameID = $coinPricePattern[$j][2]; $patternID = $coinPricePattern[$j][3];
-    echo "<option value='$patternID'>$pattern</option>";
-  }
-  echo "</select>";
-  displayTrendSymbols($comboList,'selectCmbo1Hr1', 1);
-  displayTrendSymbols($comboList,'selectCmbo1Hr2', 1);
-  displayTrendSymbols($comboList,'selectCmbo1Hr3', 1);
-  displayTrendSymbols($comboList,'selectCmbo1Hr4', 1);
-  echo "<input type='submit' name='addPricePatternBtn' value='+'>";
-  echo "<input type='submit' name='removePricePatternBtn' value='-'>";
+    echo "<select name='CoinPricePatternSelect' size='8'>";
+      for ($j=0; $j<$coinPricePatternSize; $j++){
+        $name = $coinPricePattern[$j][0]; $pattern = $coinPricePattern[$j][1];
+        $nameID = $coinPricePattern[$j][2]; $patternID = $coinPricePattern[$j][3];
+        echo "<option value='$patternID'>$pattern</option>";
+      }
+    echo "</select>";
+    displayTrendSymbols($comboList,'selectCmbo1Hr1', 1);
+    displayTrendSymbols($comboList,'selectCmbo1Hr2', 1);
+    displayTrendSymbols($comboList,'selectCmbo1Hr3', 1);
+    displayTrendSymbols($comboList,'selectCmbo1Hr4', 1);
+    echo "<input type='submit' name='addPricePatternBtn' value='+'>";
+    echo "<input type='submit' name='removePricePatternBtn' value='-'>";
   echo "</form></div>";
 
 
