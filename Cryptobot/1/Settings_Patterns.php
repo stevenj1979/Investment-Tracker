29fc8dfe39163e01309306161d7c57dc4a1bcd70
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-07-30 08:22:19 +0100

    echo sql for add pattern

--- Cryptobot/1/Settings_Patterns.php
+++ Cryptobot/1/Settings_Patterns.php
@@ -77,7 +77,7 @@ function addTrendPatterntoSQL($pattern){
   // Check connection
   if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
   $sql = "call addPricePattern('$pattern', $userID, $nameID);";
-  //echo $sql;
+  echo $sql;
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
