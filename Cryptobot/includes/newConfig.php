baf825a2e26b9c94a7bb6a8a10396d006d750258
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-08-10 13:05:22 +0100

    remove entry1

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -563,7 +563,7 @@ function getMinTradeAmount($apisecret){
   //print_r($tradeArraySize);
   $coins = getTrackingCoins();
   $coinsSize = count($coins);
-  echo "<BR> array sizes | $minTradeSize: ".$minTradeSize." coinsSize: $coinsSize entry1: ".$minTradeSize[0]["symbol"];
+  echo "<BR> array sizes | $minTradeSize: ".$minTradeSize." coinsSize: $coinsSize entry1: ";
   for ($x=0; $x<$coinsSize; $x++){
     $baseCurrency = $coins[$x][26]; $coin = $coins[$x][1]; $coinID = $coins[$x][0];
 
