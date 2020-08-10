25bdad7c864503a3317a662017c634d1ac8bb7d2
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-08-10 15:11:52 +0100

    symbol

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -571,8 +571,8 @@ function getMinTradeAmount($apisecret){
     //echo "<BR> COIN: $coin BASE: $baseCurrency ID: $coinID";
     //for($y = 0; $y < $tradeArraySize; $y++) {
     for ($y=0; $y<$tradeArraySize; $y++){
-      echo "<BR> Symbol: ".$obj[$y]['quoteCurrencySymbol']."|$coin BASE: ".$obj[$y]['baseCurrencySymbol']."|$baseCurrency Trade: ".$obj[$y]['minTradeSize'];
-      if($obj[$y]['quoteCurrencySymbol']==$coin && $obj[$y]['baseCurrencySymbol']==$baseCurrency){
+      echo "<BR> Symbol: ".$obj[$y]['symbol']."|$coin BASE: ".$obj[$y]['baseCurrencySymbol']."|$baseCurrency Trade: ".$obj[$y]['minTradeSize'];
+      if($obj[$y]['symbol']==$coin && $obj[$y]['baseCurrencySymbol']==$baseCurrency){
         $minTradeAmount = $obj[$y]['minTradeSize'];
         //return $minTradeAmount;
         echo "<BR> Coin Match: $coin Base: $baseCurrency ID: $coinID Min: $minTradeAmount";
