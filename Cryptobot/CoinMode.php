499af05a34fe8b6d3d9ef52a3c92d0362b5529c5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-10-24 18:31:29 +0100

    finish Coin Mode

--- Cryptobot/CoinMode.php
+++ Cryptobot/CoinMode.php
@@ -8,12 +8,10 @@ $apikey=getAPIKey();
 $apisecret=getAPISecret();
 $logToFileSetting = getLogToFile();
 
-function WritetoRule($coinD, $ruleID, $highPrice, $lowPrice, $buyAmount, $enable, $type){
+function WritetoRule($coinD, $ruleID, $highPrice, $lowPrice, $buyAmount, $enable, $type, $sellRuleID){
   $conn = getSQLConn(rand(1,3));
   if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
-  if ($type == 1){$sql = "";}
-  elseif ($type == 2){$sql = "";}
-  else ($type == 3){$sql = "";}
+  $sql = "call UpdateRulesforCoinMode($coinD,$ruleID,$highPrice,$lowPrice,$buyAmount,$enable,$type,$sellRuleID);";
 
   //print_r("<BR>".$sql);
   if ($conn->query($sql) === TRUE) {
@@ -30,6 +28,7 @@ function isBuyMode($coinAry){
       $coinID = $coinAry[$i][0]; $Hr24Price = $coinAry[$i][4]; $D7Price = $coinAry[$i][6];
       $Hr1AveragePrice = $coinAry[$i][11]; $month6HighPrice = $coinAry[$i][2]; $month6LowPrice = $coinAry[$i][3]; $ruleID = $coinAry[$i][1];
       $buyPrice = $coinAry[$i][9]; $livePrice = $coinAry[$i][10]; $projectedMaxPrice = $coinAry[$i][12]; $projectedMinPrice = $coinAry[$i][13];
+      $ruleIDSell = $coinAry[$i][8];
       $t1 = False; $t2 = False; $t3 = False;
       //24 Hour price is down
       if ($Hr24Price <= -5.0){ $t1 = True;}
@@ -43,7 +42,7 @@ function isBuyMode($coinAry){
         $pctToBuy = (($livePrice/$month6LowPrice)*100)/($month6HighPrice-$month6LowPrice);
         $buyAmount = ($buyPrice/100)*$pctToBuy;
         //Write Coin, High Price Limit, Low Price Limit, Buy Amount - To Rule and Enable
-        WritetoRule($coinID, $ruleID, $projectedMaxPrice,$projectedMinPrice,$buyAmount, 0, 1);
+        WritetoRule($coinID, $ruleID, $projectedMaxPrice,$projectedMinPrice,$buyAmount, 0, 1,$ruleIDSell);
       }
 
 
@@ -54,7 +53,7 @@ function isBuyMode($coinAry){
     for ($i=0; $i<$coinArySize; $i++){
         $coinID = $coinAry[$i][0]; $Hr24Price = $coinAry[$i][4]; $D7Price = $coinAry[$i][6];
         $Hr1AveragePrice = $coinAry[$i][11]; $month6HighPrice = $coinAry[$i][2]; $month6LowPrice = $coinAry[$i][3]; $ruleIDSell = $coinAry[$i][8];
-        $projectedMaxPrice = $coinAry[$i][12]; $projectedMinPrice = $coinAry[$i][13];
+        $projectedMaxPrice = $coinAry[$i][12]; $projectedMinPrice = $coinAry[$i][13]; $ruleID = $coinAry[$i][1];
         $t1 = False; $t2 = False; $t3 = False;
         //24 Hour price is up
         if ($Hr24Price >= 10){$t1 = True;}
@@ -66,7 +65,7 @@ function isBuyMode($coinAry){
           //Calculate Sell Price
 
           //Write Coin, High Price Limit, Low Price Limit  - To Rule and Enable
-          WritetoRule($coinID,$ruleIDSell,$projectedMaxPrice,$projectedMinPrice, 0, 0, 2);
+          WritetoRule($coinID,$ruleID,$projectedMaxPrice,$projectedMinPrice, 0, 0, 2,$ruleIDSell);
         }
     }
 
@@ -76,13 +75,14 @@ function isBuyMode($coinAry){
       for ($i=0; $i<$coinArySize; $i++){
           $coinID = $coinAry[$i][0]; $Hr24Price = $coinAry[$i][4]; $D7Price = $coinAry[$i][6];
           $Hr1AveragePrice = $coinAry[$i][11]; $month6HighPrice = $coinAry[$i][2]; $month6LowPrice = $coinAry[$i][3]; $ruleIDSell = $coinAry[$i][8];
+          $ruleID = $coinAry[$i][1];
           $t3 = False;
 
           //Average is Increasing
           if ($Hr1AveragePrice >= 0.5 and $Hr1AveragePrice <= -0.5){ $t3 = True;}
           if ($t3 == True){
             //Calculate Sell Price
-            WritetoRule($coinID,$ruleIDSell,0,0, 0, 0, 3);
+            WritetoRule($coinID,$ruleID,0,0, 0, 0, 3,$ruleIDSell);
             //Disable Rule
           }
 
