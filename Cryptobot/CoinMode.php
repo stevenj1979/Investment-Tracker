e805e5f25520d22956b084538378eb73e0256f5e
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-11-14 11:06:23 +0000

    minus the %

--- Cryptobot/CoinMode.php
+++ Cryptobot/CoinMode.php
@@ -86,7 +86,7 @@ function isBuyMode($coinAry, $minBuyAmount){
           if ($pctInc7Day <= -15){$newProjectedMaxPrice = $new6MonthHighPrice; $newProjectedMinPrice = $new6MonthLowPrice;}
           else{ $newProjectedMaxPrice = $projectedMaxPrice; $newProjectedMinPrice = $projectedMinPrice;}
           $numOfRisesInPrice = (10*$pctToBuy);
-          $newHighPrice = ($newProjectedMaxPrice+($newProjectedMaxPrice/50))*$pctToBuy;
+          $newHighPrice = ($newProjectedMaxPrice-($newProjectedMaxPrice/50))*$pctToBuy;
           WritetoRule($coinID, $ruleID, $newHighPrice,$newProjectedMinPrice,$buyAmount, 1, 1,$ruleIDSell,$numOfRisesInPrice,15);
           if ($modeID <> 1){
             logToSQL("CoinModeBuy","Change Coin mode to 1 for: $symbol ($coinID) | $livePrice | $new6MonthHighPrice | $new6MonthLowPrice", $userID, 1);
