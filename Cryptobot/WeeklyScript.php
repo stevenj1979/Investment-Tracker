689090439a00d52ca4b002643a29dea7d56e8a00
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-11-16 14:36:22 +0000

    add weekly script

---
+++ Cryptobot/WeeklyScript.php
@@ -0,0 +1,35 @@
+<html>
+<?php
+ini_set('max_execution_time', 300);
+require('includes/newConfig.php');
+
+include_once ('/home/stevenj1979/SQLData.php');
+include_once ('/home/stevenj1979/Encrypt.php');
+
+$apikey=getAPIKey();
+$apisecret=getAPISecret();
+
+function clearWeeklyCoinSwaps(){
+  $conn = getSQLConn(rand(1,3));
+  // Check connection
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "UPDATE `Transaction` SET `NoOfCoinSwapsThisWeek` = 0 WHERE `Status` = 'Open'";
+
+  print_r($sql);
+  if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  $conn->close();
+  logAction("clearWeeklyCoinSwaps: ".$sql, 'SellCoin', 0);
+}
+
+
+clearWeeklyCoinSwaps();
+
+?>
+</html>
