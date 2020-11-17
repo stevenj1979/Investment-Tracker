4fd5847f0b6a2089ae60d00f8673e21d9b52cff5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-11-17 10:49:44 +0000

    rounding

--- Cryptobot/1/CoinMode_Mobile.php
+++ Cryptobot/1/CoinMode_Mobile.php
@@ -35,7 +35,7 @@ if ($_SESSION['isMobile'] && $_SESSION['MobOverride'] == False){
 
 displayHeader(10);
 
-        if ($_SESSION['isMobile']){ $num = 2; $fontSize = "font-size:60px"; }else{$num = 8;$fontSize = "font-size:32px"; }
+        if ($_SESSION['isMobile']){ $num = 1; $fontSize = "font-size:60px"; }else{$num = 3;$fontSize = "font-size:32px"; }
 				$tracking = getCoinMode($_SESSION['ID']);
 				$newArrLength = count($tracking);
         echo "<TABLE><TH>Symbol</TH><TH>Mode</TH><TH>Buy Rule</TH><TH>Sell Rule</TH><TH>Secondary Sell Rules</TH><TH>1 Hr Avg Price</TH><TH>24 Hr Avg Price</TH><TH>7 Day Avg Price</TH>";
@@ -47,10 +47,10 @@ displayHeader(10);
           $symbol = $tracking[$x][36]; $coinMode = $tracking[$x][25]; $buyRule = $tracking[$x][0]; $sellRule = $tracking[$x][19];
           $secondarySellRules = $tracking[$x][32];$livePrice = $tracking[$x][2]; $Hr24Price = $tracking[$x][15];$D7Price= $tracking[$x][17];
           $month6LowPrice = $tracking[$x][4]; $month6HighPrice = $tracking[$x][3];
-          $Hr1AveragePrice = round($tracking[$x][21],3);
+          $Hr1AveragePrice = round($tracking[$x][21],$num);
           //Calculations
-          $pctInc24Hours = round((($livePrice - $Hr24Price)/$Hr24Price)*100,3);
-          $pctInc7Day = round((($livePrice - $D7Price)/$D7Price)*100,3);
+          $pctInc24Hours = round((($livePrice - $Hr24Price)/$Hr24Price)*100,$num);
+          $pctInc7Day = round((($livePrice - $D7Price)/$D7Price)*100,$num);
           if ($livePrice < $month6LowPrice){ $new6MonthLowPrice = $livePrice;} else {$new6MonthLowPrice = $month6LowPrice; }
           if ($livePrice > $month6HighPrice){ $new6MonthHighPrice = $livePrice;} else {$new6MonthHighPrice = $month6HighPrice; }
           //$pctToBuy = ($new6MonthHighPrice-$livePrice)/($new6MonthHighPrice-$new6MonthLowPrice);
@@ -61,7 +61,7 @@ displayHeader(10);
           Echo "<TD>$buyRule</TD>";Echo "<TD>$sellRule</TD>";Echo "<TD>$secondarySellRules</TD>";
           Echo "<TD>$Hr1AveragePrice</TD>";Echo "<TD>$pctInc24Hours</TD>";Echo "<TD>$pctInc7Day</TD>";
           //Echo "<TD>$livePrice</TD>";Echo "<TD>$month6HighPrice</TD>";Echo "<TD>$month6LowPrice</TD>";
-          $pctToBuy=$pctToBuy*100;
+          $pctToBuy=round($pctToBuy*100,$num);
           Echo "<TD>$pctToBuy</TD>";
           echo "<TR>";
 				}//end for
