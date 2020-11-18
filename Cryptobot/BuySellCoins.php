c102962cbef1f4f088663a4e4bf9a52b8cca6d2a
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-11-18 08:53:34 +0000

    < to >

--- Cryptobot/BuySellCoins.php
+++ Cryptobot/BuySellCoins.php
@@ -510,7 +510,7 @@ while($completeFlag == False){
       $current_date = date('Y-m-d H:i');
       $threeWeeksAgoDate = date("Y-m-d H:i",strtotime("-3 week", strtotime($current_date)));
       echo "<BR>COIN Swap: $coinSwapEnabled $noOfCoinSwapsPerWeek $noOfCoinSwapsThisWeek";
-      if ($coinSwapEnabled == 1 and $noOfCoinSwapsPerWeek < $noOfCoinSwapsThisWeek){
+      if ($coinSwapEnabled == 1 and $noOfCoinSwapsPerWeek > $noOfCoinSwapsThisWeek){
         echo "<BR>COIN Swap: $profit $orderDate $threeWeeksAgoDate";
         if ($profit < -4 and $orderDate > $threeWeeksAgoDate){
           //lookup if any Coin in Buy Mode currently
