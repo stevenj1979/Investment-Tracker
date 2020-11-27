05cab039c81225b7e0b048e3dfb9f5e008616a49
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-11-27 10:09:02 +0000

    $item[$i]["symbol"]

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -221,9 +221,9 @@ $bittrexStatsCount = count($bittrexStats);
 echo "<BR> Count: $bittrexStatsCount";
 //foreach($bittrexStats['result'] as $item) {
 for ($i=0; $i<$bittrexStatsCount; $i++){
-  echo "<BR> Symbol: ".$item[$i][0];
-  echo "<BR> high: ".$item[$i][1];
-  echo "<BR> low: ".$item[$i][2];
+  echo "<BR> Symbol: ".$item[$i]["symbol"];
+  echo "<BR> high: ".$item[$i]["high"];
+  echo "<BR> low: ".$item[$i]["low"];
 }
 
 ?>
