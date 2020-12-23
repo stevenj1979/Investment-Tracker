88441603ede111e83a3fbc212aa58c946564b0d8
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2020-12-23 20:32:49 +0000

    history every 90 mins

--- Cryptobot/CryptoBotAuto.php
+++ Cryptobot/CryptoBotAuto.php
@@ -266,7 +266,7 @@ while($date <= $newTime){
   $i = $i+1;
   //if ($i >= 2){$historyFlag = False; $marketCapFlag = Flase;}
   $date = date("Y-m-d H:i", time());
-  if (timerReady($history_date,60)){$historyFlag=True; $history_date = date('Y-m-d H:i'); Echo "<BR> History Timer ";logAction('Update History Set','CoinPrice', $logToFileSetting);}
+  if (timerReady($history_date,90)){$historyFlag=True; $history_date = date('Y-m-d H:i'); Echo "<BR> History Timer ";logAction('Update History Set','CoinPrice', $logToFileSetting);}
   if (timerReady($marketCap_date,360)){$marketCapFlag=True; $marketCap_date = date('Y-m-d H:i'); $marketCapStatsUpdateFlag = True; Echo "<BR> Market Cap Timer "; logAction('Market Cap Update Set','CoinPrice', $logToFileSetting);}
 
 }//while loop
