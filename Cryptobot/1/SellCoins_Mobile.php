b6a690be9ad19aef80dfe4eec7c5a4e25d7e50b1
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-01-12 09:38:00 +0000

    fix Sell Coin Links

--- Cryptobot/1/SellCoins_Mobile.php
+++ Cryptobot/1/SellCoins_Mobile.php
@@ -193,7 +193,7 @@ $date = date('Y/m/d H:i:s', time());
         $arrLengthSell = count($trackingSell);
         //$userConfig = getConfig($_SESSION['ID']);
         print_r("<h2>Sell Some Coins Now!</h2>");
-        echo "<h3><a href='SellCoins_Mobile.php'>Sell Coins</a> &nbsp > &nbsp <a href='SellCoins_Tracking.php'>Sell Coins Tracking</a> &nbsp > &nbsp <a href='SellCoins_Saving.php'>Sell Coins Saving</a> &nbsp > &nbsp <a href='SellCoins_Spread.php'>Sell Coins SpreadBet</a></h3>";
+        echo "<h3><a href='SellCoins.php'>Sell Coins</a> &nbsp > &nbsp <a href='SellCoins_Tracking.php'>Tracking</a> &nbsp > &nbsp <a href='SellCoins_Saving.php'>Saving</a> &nbsp > &nbsp <a href='SellCoins_Spread.php'>Spread Bet</a></h3>";
         for($x = 0; $x < $arrLengthSell; $x++) {
             //Variables
             //$roundNum = 2;
