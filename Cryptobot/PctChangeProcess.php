1fc7939f9e0660faf5005e1f4a607c08b9de79cb
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-02-06 13:54:11 +0000

    separate

--- Cryptobot/PctChangeProcess.php
+++ Cryptobot/PctChangeProcess.php
@@ -165,15 +165,24 @@ for ($i=0; $i<$coinCount; $i++){
     echo "<BR> get1HrPrice($coinID); ".$Hr1Price[0][1];
 
     //Check if 0
-    if (is_null($Hr1Price[0][1]) OR $Hr1Price[0][1] == 0){
+    if (is_null($Hr1Price[0][1])){
        $CMCStats = getCMCstats($CMCStats, $coinStr);
        $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
        $price1Hr = $tempPrice[0][2];
+    }elseif ($Hr1Price[0][1] == 0){
+      $CMCStats = getCMCstats($CMCStats, $coinStr);
+      $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
+      $price1Hr = $tempPrice[0][2];
     }else{
       $price1Hr = $Hr1Price[0][1];
     }
+
     $Hr24Price = get24HrPrice($coinID);
-    if (is_null($Hr24Price[0][1]) OR $Hr24Price[0][1] == 0){
+    if (is_null($Hr24Price[0][1])){
+      $CMCStats = getCMCstats($CMCStats, $coinStr);
+      $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
+      $price1Hr = $tempPrice[0][3];
+    }elseif ($Hr24Price[0][1] == 0){
       $CMCStats = getCMCstats($CMCStats, $coinStr);
       $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
       $price1Hr = $tempPrice[0][3];
@@ -182,13 +191,18 @@ for ($i=0; $i<$coinCount; $i++){
     }
 
     $D7Price = get7DPrice($coinID);
-    if (is_null($D7Price[0][1]) OR $D7Price[0][1] == 0){
+    if (is_null($D7Price[0][1])){
        $CMCStats = getCMCstats($CMCStats, $coinStr);
        $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
        $price7D = $tempPrice[0][3];
+    }elseif ($D7Price[0][1] == 0){
+      $CMCStats = getCMCstats($CMCStats, $coinStr);
+      $tempPrice = findCoinStats($CMCStats,$coins[$i][1]);
+      $price7D = $tempPrice[0][3];
     }else{
       $price7D = $D7Price[0][1];
     }
+    
     //Write to PricePctChangeHistory
     writePctPrices($coinID, $price1Hr, $price24Hr, $price7D);
     echo "<BR> write1HrPrice($coinID, $price1Hr, $price24Hr, $price7D);";
