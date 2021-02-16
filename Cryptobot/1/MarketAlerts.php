031b25e2efe5ec094443247988083e4e263a9a4a
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-02-16 07:49:05 +0000

    remove symbol and coinID

--- Cryptobot/1/MarketAlerts.php
+++ Cryptobot/1/MarketAlerts.php
@@ -21,15 +21,14 @@ include_once ('/home/stevenj1979/SQLData.php');
 
 setStyle($_SESSION['isMobile']);
 
-if (isset($_get[‘alert’])){
+if (isset($_get['alert'])){
 
 }else{
 	showMain();
 }
 
 Function  getMarketAlertsUser($userID){
-
-$conn = getSQLConn(rand(1,3));
+  $conn = getSQLConn(rand(1,3));
   if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
   $sql = "SELECT `ID`, `Action`, `Price`, `UserName`,`Email` ,`LiveCoinPrice`,`Category`,`Hr1PctChange` ,`Hr24PctChange` ,`D7PctChange`,`ReocurringAlert`,`DateTimeSent`
   FROM `MarketAlertsView` WHERE `UserID` = $userID";
@@ -41,7 +40,6 @@ $conn = getSQLConn(rand(1,3));
   }
   $conn->close();
   return $tempAry;
-
 }
 
 Function showMain(){
@@ -50,20 +48,19 @@ displayHeader(8);
   if ($_SESSION['isMobile']){ $num = 2; $fontSize = "font-size:60px"; }else{$num = 8;$fontSize = "font-size:32px"; }
   NewEcho("<h2>Coin Alerts!</h2>",$_SESSION['isMobile'] ,2);
   echo "<h3><a href='CoinAlerts.php'>Coin Alerts</a> &nbsp > &nbsp <a href='MarketAlerts.php'>Market Alerts</a></h3>";
-  NewEcho("<Table><th>Edit</th><th>&nbspID</th><TH>&nbspCoinID</th><TH>&nbspAction</th><TH>&nbspPrice</th><TH>&nbspSymbol</th>",$_SESSION['isMobile'] ,2);
+  NewEcho("<Table><th>Edit</th><th>&nbspID</th><TH>&nbspAction</th><TH>&nbspPrice</th>",$_SESSION['isMobile'] ,2);
   newEcho("<TH>&nbspUserName</th><TH>&nbspEmail</th>",$_SESSION['isMobile'] ,0);
   newEcho("<TH>&nbspliveCoinPrice</th><TH>&nbspCategory</th><th>Reocurring</th><TH>&nbspDelete Alert</th><tr>",$_SESSION['isMobile'] ,2);
   $coinAlerts = getMarketAlertsUser($userID);
   $newArrLength = Count($coinAlerts);
   for($x = 0; $x < $newArrLength; $x++) {
-    $id = $coinAlerts[$x][0];$coinID = $coinAlerts[$x][1]; $action = $coinAlerts[$x][2];
-    $price = round($coinAlerts[$x][3],$roundNum);$symbol = $coinAlerts[$x][4]; $userName = $coinAlerts[$x][5];
+    $id = $coinAlerts[$x][0]; $action = $coinAlerts[$x][2];
+    $price = round($coinAlerts[$x][3],$roundNum); $userName = $coinAlerts[$x][5];
     $email = $coinAlerts[$x][6];$liveCoinPrice= round($coinAlerts[$x][7],$roundNum); $category = $coinAlerts[$x][8];
     $reocurring = $coinAlerts[$x][12]; $coinAlertRuleID = $coinAlerts[$x][14];
     NewEcho("<td><a href='CoinAlerts.php?alert=1&edit=".$coinAlertRuleID."'><span class='glyphicon glyphicon-pencil' style='$fontSize;'></span></a></td>",$_SESSION['isMobile'] ,2);
-    NewEcho("<td>$id</td><td>$coinID</td>",$_SESSION['isMobile'] ,2);
+    NewEcho("<td>$id</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$action</td><td>$price</td>",$_SESSION['isMobile'] ,2);
-    NewEcho("<td>$symbol</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$userName</td><td>$email</td>",$_SESSION['isMobile'] ,0);
     NewEcho("<td>$liveCoinPrice</td><td>$category</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$reocurring</td>",$_SESSION['isMobile'] ,2);
