b54a852b5805e708f55df4437c67aba3330c81fb
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-02-21 16:09:18 +0000

    add $id

--- Cryptobot/1/CoinAlerts.php
+++ Cryptobot/1/CoinAlerts.php
@@ -192,14 +192,14 @@ if ($_GET['alert'] == 0 && isset($_GET['alert'])){
     $price = round($coinAlerts[$x][3],$roundNum);$symbol = $coinAlerts[$x][4]; $userName = $coinAlerts[$x][5];
     $email = $coinAlerts[$x][6];$liveCoinPrice= round($coinAlerts[$x][7],$roundNum); $category = $coinAlerts[$x][8];
     $reocurring = $coinAlerts[$x][12]; //$coinAlertRuleID = $coinAlerts[$x][14];
-    NewEcho("<td><a href='CoinAlerts.php?alert=1&edit=".$coinAlertRuleID."'><span class='glyphicon glyphicon-pencil' style='$fontSize;'></span></a></td>",$_SESSION['isMobile'] ,2);
+    NewEcho("<td><a href='CoinAlerts.php?alert=1&edit=".$id."'><span class='glyphicon glyphicon-pencil' style='$fontSize;'></span></a></td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$id</td><td>$coinID</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$action</td><td>$price</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$symbol</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$userName</td><td>$email</td>",$_SESSION['isMobile'] ,0);
     NewEcho("<td>$liveCoinPrice</td><td>$category</td>",$_SESSION['isMobile'] ,2);
     NewEcho("<td>$reocurring</td>",$_SESSION['isMobile'] ,2);
-    NewEcho("<td><a href='CoinAlerts.php?alert=4&iD=$coinAlertRuleID'><i class='glyphicon glyphicon-trash' style='$fontSize;color:#D4EFDF'></i></a></td>",$_SESSION['isMobile'] ,2);
+    NewEcho("<td><a href='CoinAlerts.php?alert=4&iD=$id'><i class='glyphicon glyphicon-trash' style='$fontSize;color:#D4EFDF'></i></a></td>",$_SESSION['isMobile'] ,2);
     NewEcho("<TR>",$_SESSION['isMobile'] ,2);
   }
   Echo "</table>";
