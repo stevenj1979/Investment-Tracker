336414ead7b4a12ab2583d17c7bb933d6020ccae
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-02-24 19:11:34 +0000

    pct < 100

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -255,9 +255,9 @@ function getPctChangeFromHistory(){
   $sql = "SELECT `CoinID`,min(`Hr1Pct`) as Hr1Pct, min(`Hr24Pct`) as Hr24Pct, min(`D7Pct`) as D7Pct, Month(`PriceDate`) as Month,Year(`PriceDate`) as Year  FROM `PriceHistory`
 WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
 AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-and `Hr1Pct` > -200 and `Hr1Pct` < 200
-and `Hr24Pct` > -200 and `Hr24Pct` < 200
-and `D7Pct` > -200 and `D7Pct` < 200
+and `Hr1Pct` > -100 and `Hr1Pct` < 100
+and `Hr24Pct` > -100 and `Hr24Pct` < 100
+and `D7Pct` > -100 and `D7Pct` < 100
 group by `CoinID`";
   $result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
