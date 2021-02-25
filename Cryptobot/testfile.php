8a4c859d0b865ce77c21a021b4fb4f759a18b7f6
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-02-25 07:54:42 +0000

    getAvgPctChangeFromHistory

--- Cryptobot/testfile.php
+++ Cryptobot/testfile.php
@@ -270,13 +270,13 @@ group by `CoinID`";
   return $tempAry;
 }
 
-$pctData = getPctChangeFromHistory();
+$pctData = writeAvgPctDatatoSQL();
 $pctDataSize = count($pctData);
 for ($i=0;$i<$pctDataSize; $i++){
   $coinID = $pctData[$i][0]; $hr1Price = $pctData[$i][1]; $hr24Price = $pctData[$i][2]; $d7Price = $pctData[$i][3];
   $month = $pctData[$i][4]; $year = $pctData[$i][5];
   echo "<BR>writePctDatatoSQL($coinID,$hr1Price,$hr24Price,$d7Price,$month,$year);";
-  writePctDatatoSQL($coinID,$hr1Price,$hr24Price,$d7Price,$month,$year);
+  getAvgPctChangeFromHistory($coinID,$hr1Price,$hr24Price,$d7Price,$month,$year);
 }
 
 ?>
