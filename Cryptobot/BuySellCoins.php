e0af5c78587b300836a60fa1c0ebedd206e1aac5
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-04-29 19:38:12 +0100

    revert BTCAmount

--- Cryptobot/BuySellCoins.php
+++ Cryptobot/BuySellCoins.php
@@ -195,41 +195,41 @@ while($completeFlag == False){
     //if (($coinMode == 0) and ($ruleIDBuy > 0) and ($coinAllocation[0][1]<$BTCAmount)){continue;}
     if ($coinMode > 0){
       if ($coinAllocation[0][1]<$BTCAmount){
-        if ($coinAllocation[0][1] <= 0){
+        //if ($coinAllocation[0][1] <= 0){
           echo "<BR> EXIT1: $coinMode | $type | $BTCAmount | ".$coinAllocation[0][1];
            continue;
-        }else{
-          $BTCAmount = $coinAllocation[0][1];
-          $indexLookup = 1;
-        }
-      }
+        //}else{
+          //$BTCAmount = $coinAllocation[0][1];
+          //$indexLookup = 1;
+        //}
+      }else{ $indexLookup = 1;}
     }elseif ($coinMode == 0 AND $type == 'SpreadBuy'){
       if ($coinAllocation[0][2]<$BTCAmount){
-        if ($coinAllocation[0][2] <= 0){
+        //if ($coinAllocation[0][2] <= 0){
           echo "<BR> EXIT2: $coinMode | $type | $BTCAmount | ".$coinAllocation[0][2];
           continue;
-        }else{
-          $BTCAmount = $coinAllocation[0][2];
-          $indexLookup = 3;
-        }
-      }
+        //}else{
+          //$BTCAmount = $coinAllocation[0][2];
+          //$indexLookup = 3;
+        //}
+      }else{ $indexLookup = 3;}
     }elseif ($coinMode == 0 AND $type == 'Buy'){
       if ($coinAllocation[0][0]<$BTCAmount){
-        if ($coinAllocation[0][0] <= 0){
+        //if ($coinAllocation[0][0] <= 0){
           echo "<BR> EXIT3: $coinMode | $type | $BTCAmount | ".$coinAllocation[0][0];
           continue;
-        }else{
-          $BTCAmount = $coinAllocation[0][0];
-          $indexLookup = 2;
-        }
-      }
+        //}else{
+          //$BTCAmount = $coinAllocation[0][0];
+
+        //}
+      }else{ $indexLookup = 2;}
     }
     $openTransactionsSize = count($openTransactions);
     for ($h=0; $h<$openTransactionsSize; $h++){
       if ($openTransactions[$h][0] == $userID){
         $oldBTCAmount = $BTCAmount;
         $liveOpenTrans = $openTransactions[$h][$indexLookup];
-        $BTCAmount = $BTCAmount / ($liveOpenTrans-$noOfBuys);
+        //$BTCAmount = $BTCAmount / ($liveOpenTrans-$noOfBuys);
         LogToSQL("TrackingCoin","BTC Alloction: $oldBTCAmount | $BTCAmount | $indexLookup | $liveOpenTrans | $noOfBuys",3,1);
       }
     }
