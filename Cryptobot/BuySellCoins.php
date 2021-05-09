80a3ebe1cc7e3b1c5186b81ff1f1fb9389a7b69b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-05-09 19:05:49 +0100

    $totalBal = ($BTCBalance*$baseMultiplier[0][0])-$reservedAmount[0][0];

--- Cryptobot/BuySellCoins.php
+++ Cryptobot/BuySellCoins.php
@@ -614,10 +614,10 @@ while($completeFlag == False){
         //if ($reservedAmount <> 0){
         Echo "<BR> TEST BAL AND RES: $BTCBalance ; ".$reservedAmount[0][0]." | "; //.$BTCBalance-$reservedAmount
         if ($baseCurrency == 'BTC'){
-          $totalBal = ($BTCBalance-$reservedAmount[0][0])*$baseMultiplier[0][0];
+          $totalBal = ($BTCBalance*$baseMultiplier[0][0])-$reservedAmount[0][0];
           LogToSQL("BTCTest","BaseCurrency is BTC : totalBal: $totalBal | Multiplier : ".$baseMultiplier[0][0],3,1);
         }elseif ($baseCurrency == 'ETH'){
-          $totalBal = ($BTCBalance-$reservedAmount[0][0])*$baseMultiplier[0][1];
+          $totalBal = ($BTCBalance * $baseMultiplier[0][1])-$reservedAmount[0][0];
           LogToSQL("ETHTest","BaseCurrency is ETH : totalBal: $totalBal | Multiplier : ".$baseMultiplier[0][1],3,1);
         }else{
           $totalBal = $BTCBalance-$reservedAmount[0][0];
