c41119309457c1e57038044761432dfba4d6a0d0
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-05-31 14:41:32 +0100

    .

--- Cryptobot/1/SellCoins_Tracking.php
+++ Cryptobot/1/SellCoins_Tracking.php
@@ -98,11 +98,11 @@ if(isset($_GET['SellNow'])){
           NewEcho ("<td>|$livePriceUSD</td>",$_SESSION['isMobile'],0);
           NewEcho ("<td>|".Round($LiveCoinPrice,$num)."</td>",$_SESSION['isMobile'],2);
           NewEcho ("<td>|".number_format($profit,$num)."</td>",$_SESSION['isMobile'],2);
-          NewEcho ("<td>|"number_format($fee,$num)."</td>",$_SESSION['isMobile'],0);
-          NewEcho ("<td>|"number_format($ProfitPct,$num)."</td>",$_SESSION['isMobile'],2);
-          NewEcho ("<td>|"number_format($baseSellPrice,$num)."</td>",$_SESSION['isMobile'],2);
+          NewEcho ("<td>|".number_format($fee,$num)."</td>",$_SESSION['isMobile'],0);
+          NewEcho ("<td>|".number_format($ProfitPct,$num)."</td>",$_SESSION['isMobile'],2);
+          NewEcho ("<td>|".number_format($baseSellPrice,$num)."</td>",$_SESSION['isMobile'],2);
           NewEcho ("<td>|$NoOfRisesInPrice / $totalRisesInPrice</td>",$_SESSION['isMobile'],0);
-          NewEcho ("<td>|"number_format($ogPctProfit,$num)."</td>",$_SESSION['isMobile'],2);
+          NewEcho ("<td>|".number_format($ogPctProfit,$num)."</td>",$_SESSION['isMobile'],2);
           NewEcho ("<td><a href='SellCoins_Tracking.php?CancelTrack=Yes&TransID=$TransactionID'><i class='fas fa-ban' style='$fontSize;color:DodgerBlue'></i></a></td>",$_SESSION['isMobile'],2);
           NewEcho ("<td><a href='SellCoins_Tracking.php?SellNow=Yes&TransID=$TransactionID'><i class='fas fa-shopping-cart' style='$fontSize;color:DodgerBlue'></i></a></td>",$_SESSION['isMobile'],2);
           echo "</tr>";
