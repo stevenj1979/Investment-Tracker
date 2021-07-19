030b5c6105b640495897cabba8c305b5e3579379
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-07-19 20:35:55 +0100

    crate CoinSwap File

---
+++ Cryptobot/CoinSwap.php
@@ -0,0 +1,18 @@
+<html>
+<?php
+ini_set('max_execution_time', 300);
+require('includes/newConfig.php');
+
+include_once ('/home/stevenj1979/SQLData.php');
+include_once ('/home/stevenj1979/Encrypt.php');
+
+$apikey=getAPIKey();
+$apisecret=getAPISecret();
+
+
+
+// MAIN PROGRAMME
+
+
+?>
+</html>
