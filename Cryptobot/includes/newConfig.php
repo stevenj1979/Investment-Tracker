92cb370542eac88b7d2f4d0ef2ae8f050e5c0109
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-10-04 10:16:25 +0100

    echo sql

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -2278,7 +2278,7 @@ function bittrexBuyCancel($bittrexRef, $transactionID){
   }
   $conn->close();
   logAction("bittrexBuyCancel: ".$sql, 'BuySell', 0);
-  newLogToSQL("bittrexBuyCancel","$sql",3,0,"SQL CALL","BittrexRef:$bittrexRef TransactionID:$transactionID");
+  newLogToSQL("bittrexBuyCancel","$sql",3,1,"SQL CALL","BittrexRef:$bittrexRef TransactionID:$transactionID");
 }
 
 function bittrexBuyComplete($bittrexRef,$transactionID, $finalPrice){
