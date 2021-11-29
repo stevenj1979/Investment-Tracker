81a627916ca221704c67c8bec3e98844a0a84b03
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-11-29 18:19:06 +0000

    new echo

--- Cryptobot/1/bittrexOrders.php
+++ Cryptobot/1/bittrexOrders.php
@@ -67,7 +67,7 @@ function getBTTrackingCoins($userID){
   $sql = "SELECT `Type`,`BittrexRef`,`ActionDate`,`CompletionDate`,`Status`,`SellPrice`,`UserName`,`APIKey`,`APISecret`,`Symbol`,`Amount`,`CoinPrice`,`UserID`,`Email`,`OrderNo`,
           `TransactionID`,`BaseCurrency`,`LiveCoinPrice`,`QuantityFilled`,`KEK`
   FROM `View4_BittrexBuySell` WHERE `userID` = $userID and ".$statusA.$sqlOption.$statusB." order by `ActionDate` desc limit 50";
-  //echo "<BR>$sql";
+  echo "<BR>$sql";
   $result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
 //mysqli_fetch_assoc($result);
