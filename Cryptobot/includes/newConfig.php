abf79fee1f809679ade26422022811e3d63c481c
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-12-28 22:16:02 +0000

    if ($BTCBalance < $btcBuyAmount){

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -494,7 +494,13 @@ function returnBuyAmount($coin, $baseCurrency, $btcBuyAmount, $buyType, $BTCBala
     }elseif ($btcBuyAmount > 0 && $buyType == 1){
       echo "<BR> 3: ($btcBuyAmount) ";
       //$returnPrice = ($BTCBalance*($btcBuyAmount/100))- (($BTCBalance/ 100 ) * 0.28);
-      $returnPrice = $btcBuyAmount/$bitPrice;
+      if ($BTCBalance < $btcBuyAmount){
+          $tempFee = ($BTCBalance/100)*0.28;
+          $returnPrice = ($BTCBalance-$tempFee)/$bitPrice;
+      }else{
+        $returnPrice = $btcBuyAmount/$bitPrice;  
+      }
+
       echo " $returnPrice ";
       $testFlag = 3;
     }
