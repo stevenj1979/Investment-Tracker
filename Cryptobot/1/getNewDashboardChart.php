8e189d8c32415930671ece79aeeff3c1f48066dc
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2021-12-30 13:13:18 +0000

    ActionDate

--- Cryptobot/1/getNewDashboardChart.php
+++ Cryptobot/1/getNewDashboardChart.php
@@ -48,7 +48,7 @@ $result = $conn->query($query);
 while ($row = mysqli_fetch_assoc($result)){
     $temp = array();
     // each column needs to have data inserted via the $temp array
-    $temp[] = array('v' => $row['Date']);
+    $temp[] = array('v' => $row['ActionDate']);
     //$temp[] = array('v' => (float) $row['TotalBTC']*$btcPrice);
     $temp[] = array('v' => $row['BaseCurrency']);
     //$temp[] = array('v' => (float) $row['TotalUSDT']*$usdtPrice);
