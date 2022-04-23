85800fcb96bb92fd5a698ce50ac21a24bf84b85f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-04-23 17:43:05 +0100

    $sym

--- Cryptobot/1/AddNewSetting.php
+++ Cryptobot/1/AddNewSetting.php
@@ -823,7 +823,7 @@ function displayEdit($id){
     for ($w=0; $w<$coinListSize;$w++){
       $limitCoinID = $coinList[$w][0];   $sym = $coinList[$w][1]; $base = $coinList[$w][2];
       $symBase = $sym & "-" & $base;
-      if ($formSettings[0][45] == $coinList[1][$w]){
+      if ($formSettings[0][45] == $sym){
           echo "<option value='$limitCoinID' selected>$symBase</option>";
       }else{
           echo "<option value='$limitCoinID' >$symBase</option>";
