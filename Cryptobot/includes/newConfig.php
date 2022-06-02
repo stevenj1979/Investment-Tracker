99e1395feb5d5e306948656da64e3861ae43e291
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-06-02 19:27:54 +0100

    var dump

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -1434,6 +1434,7 @@ function bittrexCoinPrice($apikey, $apisecret, $baseCoin, $coin, $versionNum){
         $balance = curl_exec($ch);
         curl_close($ch);
         $temp = json_decode($balance, true);
+        var_dump($temp);
         $balance = $temp['lastTradeRate'];
       }
 
