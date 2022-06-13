9785792b07ad0861013da304303d5e1f4ee86891
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-06-13 22:18:40 +0100

    move

--- Cryptobot/CoinMode.php
+++ Cryptobot/CoinMode.php
@@ -51,8 +51,9 @@ function checkMarketforPctDip(){
   $marketStatsSize = count($marketStats);
   for ($y=0; $y<$marketStatsSize; $y++){
     $marketPctChangeHr1 = $marketStats[$y][0]; $marketPctChangeHr24 = $marketStats[$y][1];$marketPctChangeD7 = $marketStats[$y][2];
-    echo "<BR> Checking: 1Hr: $marketPctChangeHr1 | 24Hr: $marketPctChangeHr24 | 7D: $marketPctChangeD7 TotalUserID: $userIDsSize LowMarketStartPct:$lowMarketModeStartPct Inc:$lowMarketModeIncrements";
     $lowMarketModeStartPct = $userIDs[0][1]; $lowMarketModeIncrements = $userIDs[0][2];
+    echo "<BR> Checking: 1Hr: $marketPctChangeHr1 | 24Hr: $marketPctChangeHr24 | 7D: $marketPctChangeD7 TotalUserID: $userIDsSize LowMarketStartPct:$lowMarketModeStartPct Inc:$lowMarketModeIncrements";
+
     if ($marketPctChangeHr24 <= $lowMarketModeStartPct){
         for ($t=0; $t<$userIDsSize; $t++){
           $userID = $userIDs[$t][0];
