4945ca28ae86b29e8fdb3745fb01334bf4d5915f
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-06-15 15:40:39 +0100

    exit 1

--- Cryptobot/NewBuySellCoins.php
+++ Cryptobot/NewBuySellCoins.php
@@ -1285,7 +1285,7 @@ function runSellCoins($sellRules,$sellCoins,$userProfit,$coinPriceMatch,$coinPri
       if ($multiSellRuleEnabled == 1){
           $multiSellRules = getMultiSellRules($transactionID);
           $multiSellResult = checkMultiSellRules($ruleIDSell,$multiSellRules);
-          if ($multiSellResult == False){ continue;} //echo "Exit: No1 | $coin | $userID | $BuyRule";
+          if ($multiSellResult == False){ echo "Exit: No1 | $coin | $userID | $ruleIDSell | $multiSellResult";continue;} //echo "Exit: No1 | $coin | $userID | $BuyRule";
       }else{
           if ($fixSellRule != "ALL" && (int)$fixSellRule != $ruleIDSell){continue;}//else{Echo "<BR> HERE4!";}  //echo "Exit: No2 | $coin | $userID | $BuyRule";
       }
