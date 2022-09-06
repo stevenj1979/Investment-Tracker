8e9339e669ea3396e4501388123a74fe3271428e
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-09-06 10:56:57 +0100

    and

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -163,7 +163,7 @@ function reopenCoinSwapCancel($transID, $nFlag){
     if ($nFlag == 1){
         $sql = "UPDATE `BuyBack` SET `Status` = 'Open' WHERE `TransactionID` = (SELECT `OldBuyBackTransID` FROM `BittrexAction` WHERE `ID` = $transID) and `TransactionID` <> 0";
     }else{
-      $sql = "UPDATE `BuyBack` SET `Status` = 'Open' WHERE `TransactionID` = $transID where `TransactionID` <> 0";
+      $sql = "UPDATE `BuyBack` SET `Status` = 'Open' WHERE `TransactionID` = $transID and `TransactionID` <> 0";
     }
 
     print_r($sql);
