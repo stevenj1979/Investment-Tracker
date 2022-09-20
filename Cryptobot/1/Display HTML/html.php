a1f9a6a04d9158a969fe6b77ccd028b16cc88b47
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-09-20 10:00:42 +0100

    include_once

--- Cryptobot/1/Display HTML/html.php
+++ Cryptobot/1/Display HTML/html.php
@@ -3,7 +3,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 </head>
-<?php require('includes/config.php');
+<?php
+include_once('includes/config.php');
 include_once '../includes/newConfig.php';
 
 //if not logged in redirect to login page
