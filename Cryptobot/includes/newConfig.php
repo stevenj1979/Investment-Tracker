8f2ad73e9a96cf8082a1cdc491e61449bee8a113
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-09-28 11:36:51 +0100

    writeCalculatedSellPct

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -5901,7 +5901,7 @@ function writeCalculatedSellPct($transID, $userID,$sellPct){
   }
   $conn->close();
   logAction("writeCalculatedSellPct: ".$sql, 'SQL_CALL', 0);
-  newLogToSQL("writeCalculatedSellPct","$sql",3,1,"SQL CALL","UserID:$userID TransID:$transID");
+  newLogToSQL("writeCalculatedSellPct","$sql",3,0,"SQL CALL","UserID:$userID TransID:$transID");
 }
 
 function getCoinAllocation($userID){
