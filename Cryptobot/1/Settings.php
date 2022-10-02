6671474ef6ad0ab66037219e3d02ebe37c84df0b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2022-10-02 13:36:16 +0100

    SQL

--- Cryptobot/1/Settings.php
+++ Cryptobot/1/Settings.php
@@ -187,7 +187,7 @@ function updateUser($settingsUpdateAry){
          UPDATE `User` SET `UserName`='$newusername',`Email`='$email' WHERE `ID` = $userID;
          UPDATE `ReduceLossSettings` SET `Enabled`= $reduceLossEnabled, `SellPct` = $sellPct, `OriginalPriceMultiplier` = $originalPriceMultiplier, `ReduceLossMaxCounter` = $reduceLossMaxCounter, `HoursFlat` = $reduceLossHoursFlat WHERE `UserID` = $userID;
          UPDATE `NewCoinAllocations` SET `USDTAlloc` = $usdtAlloc,`BTCAlloc` = $btcAlloc, `ETHAlloc` = $ethAlloc, `PctOnLow` = $pctOnLow WHERE `UserID` = $userID";
-  //print_r("<br>".$sql."<br>");
+  print_r("<br>".$sql."<br>");
   if ($conn->multi_query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
