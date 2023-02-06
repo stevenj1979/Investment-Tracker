7093a3d748493d23450f41c3706aa8466b8da11c
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-02-06 09:50:18 +0000

    < $buyBackPct

--- Cryptobot/NewBuySellCoins.php
+++ Cryptobot/NewBuySellCoins.php
@@ -304,7 +304,7 @@ function runBuyBack($buyBackCoins){
     if ($buyBackHoursFlatAutoEnabled == 1){
       $hoursFlatTarget = floor(($maxHoursFlat/100)*$pctOfAuto);
     }
-    if ($profitPct < -5){
+    if ($profitPct < $buyBackPct){
       //$pctOfAuto = 100 + $profitPct;
       $hoursFlatTarget = floor(($maxHoursFlat/100)*(($pctOfAuto/100)*Abs(100+$profitPct)));
     }
