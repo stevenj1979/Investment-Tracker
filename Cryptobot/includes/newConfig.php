94d77c9b20163bcfe0fb078a7cf1dd40a3a4d1be
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-02-28 10:08:57 +0000

    $sql

--- Cryptobot/includes/newConfig.php
+++ Cryptobot/includes/newConfig.php
@@ -3600,7 +3600,7 @@ function SuperLog($nFile, $comments,  $nFunction, $ref, $logSettingAry){
   //echo "<BR> Comments/Ref: $comments | $ref";
   $sql = "call superLogToSQL($UserID,'$nFile','$comments',$days,'$nFunction','$ref')";
 
-  $sql = str_replace("'","/",$comments);
+  $sql = str_replace("'","/",$sql);
   echo "<BR> SQL TEST: START $sql END";
   if ($enabled == 1) {
     if ($mode > 0){
