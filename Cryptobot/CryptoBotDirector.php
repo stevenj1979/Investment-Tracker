7ab95e91f21c32b7df1cbaf14c26e0a19a07a100
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-03-04 21:17:44 +0000

    only 5 seconds

--- Cryptobot/CryptoBotDirector.php
+++ Cryptobot/CryptoBotDirector.php
@@ -13,8 +13,15 @@ $logToSQLSetting = getLogToSQL();
 $GLOBALS['logToSQLSetting'] = getLogToSQL();
 $GLOBALS['logToFileSetting'] = getLogToFile();
 
+$programRunTime = "+5 seconds";
+if (!empty($argv[1])){
+  parse_str($argv[1], $params);
+  $programRunTime = str_replace('_', ' ', $params['mins']);
+  //echo $tmpTime;
+  //error_log($argv[1], 0);
+}
 //Program
-$programRunTime = "+30 minutes";
+//$programRunTime = "+30 minutes";
 $newTime = date("Y-m-d H:i",strtotime($programRunTime, strtotime(date('Y-m-d H:i'))));
 //AllCoinStatus
 $allCoinsStatusRunTime = "+20 minutes";
