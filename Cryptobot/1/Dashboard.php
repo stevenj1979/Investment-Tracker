633e4e5fc55b2f507fbc1c75b905353f87d6ae67
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-04-19 14:08:56 +0100

    header.php update style

--- Cryptobot/1/Dashboard.php
+++ Cryptobot/1/Dashboard.php
@@ -100,7 +100,7 @@ $title = 'CryptoBot';
 $current_url = $_SERVER[ 'REQUEST_URI' ];
 header( "Refresh: 120; URL=$current_url" );
 //include header template
-require('layout/header.php');
+//require('layout/header.php');
 //include_once ('../../../../SQLData.php');
 $locationStr = "Location: /Investment-Tracker/Cryptobot/1/m/SellCoins.php";
 
