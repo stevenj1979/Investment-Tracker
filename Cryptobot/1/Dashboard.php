0b55a030b943b10131939312bb3ac8a25a0bd19b
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-04-20 15:24:13 +0100

    dash title

--- Cryptobot/1/Dashboard.php
+++ Cryptobot/1/Dashboard.php
@@ -8,7 +8,7 @@ include '../includes/newConfig.php';
 ?>
 
 
-  <?php newHeaderHTML(); ?>
+  <?php newHeaderHTML('Dashboard'); ?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
