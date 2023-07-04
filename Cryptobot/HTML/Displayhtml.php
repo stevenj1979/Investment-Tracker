737237d358be29a9b71765902e09e3cae3ac11a3
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-07-04 09:42:38 +0100

    add image

--- Cryptobot/HTML/Displayhtml.php
+++ Cryptobot/HTML/Displayhtml.php
@@ -6,8 +6,15 @@ function displayBox($boxAry){
   $boxArySize = count($boxAry);
   echo "<div class='flex-CoinBox'>";
   for ($row = 0; $row < $boxArySize; $row++) {
-    $name = $boxAry[$row][0]; $data = $boxAry[$row][1]; $size = $boxAry[$row][2];
-      echo "<DIV class='flex-CoinBoxItem'>name: $name | data: $data | size: $size </DIV>";
+    $name = $boxAry[$row][0]; $data = $boxAry[$row][1]; $size = $boxAry[$row][3]; $image = $boxAry[$row][2];
+      if ($name == "Image"){
+        echo "<DIV class='flex-CoinBoxItem'><a href='$data'><img src='$image'></a> </DIV>";
+      }elseif ($name == "Link"){
+        echo "<DIV class='flex-CoinBoxItem'><a href='$data'>$name</a> </DIV>";
+      }else{
+        echo "<DIV class='flex-CoinBoxItem'>$data</DIV>";
+      }
+
   }
   echo "</DIV>";
 }
