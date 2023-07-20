625b4b94da5b7fcb7116d007976e9a81a3a4cde4
stevenj1979 <32767786+stevenj1979@users.noreply.github.com>
2023-07-20 15:45:14 +0100

    monthly High Low

--- Cryptobot/MonthlyHighLow.php
+++ Cryptobot/MonthlyHighLow.php
@@ -4,166 +4,175 @@ ini_set('max_execution_time', 900);
 require('includes/newConfig.php');
 //require '/home/stevenj1979/repositories/Sparkline/autoload.php';
 include_once ('/home/stevenj1979/SQLData.php');
+include_once ('includes/SQLDbCommands.php');
 $apikey=getAPIKey();
 $apisecret=getAPISecret();
 $logToFileSetting = getLogToFile();
 
 
 function getLastMonthCoinPrice(){
-
-  $conn = getHistorySQL(rand(1,4));
-  if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}
+  $tempAry = [];
+  /*$conn = getHistorySQL(rand(1,4));
+  if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}*/
   $sql = "SELECT `Lmhp`.`CoinID`,`Lmhp`.`MonthHighPrice` as MonthHighPrice,`Lmhp`.`Month`,`Lmhp`.`Year`,  `Lmmp`.`MonthLowPrice` as MonthLowPrice
   FROM `LastMonthHighPrice` `Lmhp`
 	join `LastMonthLowPrice` `Lmmp` on `Lmmp`.`CoinID` = `Lmhp`.`CoinID` and `Lmmp`.`Month` = `Lmhp`.`Month` and `Lmmp`.`Year` = `Lmhp`.`Year`
   where `Lmhp`.`MonthHighPrice` <> 0 and `Lmmp`.`MonthLowPrice` <> 0  ";
-  echo "<BR>".$sql;
-  $result = $conn->query($sql);
+  //echo "<BR>".$sql;
+  $tempAry = mySQLSelect("getLastMonthCoinPrice: ",$sql,3,1,1,1,"NewConfig",90);
+  /*$result = $conn->query($sql);
   while ($row = mysqli_fetch_assoc($result)){
     $tempAry[] = Array($row['CoinID'],$row['MonthHighPrice'],$row['Month'],$row['Year'],$row['MonthLowPrice']);
   }
-  $conn->close();
+  $conn->close();*/
 return $tempAry;
 }
 
 function writePrice($coinID, $price, $month, $year, $minPrice){
-  $conn = getSQLConn(rand(1,3));
-  if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+  /*$conn = getSQLConn(rand(1,3));
+  if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}*/
   $sql = "call UpdateMonthlyMinMaxPrice($coinID,$minPrice,$price,$month,$year);";
-  //print_r("<BR>".$sql);
+  SQLInsertUpdateCall("writePrice: ",$sql,3, 1, 1, 0, "NewConfig", 90);
+  /*print_r("<BR>".$sql);
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
-  $conn->close();
+  $conn->close();*/
 }
 
 function writePctDatatoSQL($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year){
-  $conn = getSQLConn(rand(1,3));
+  /*$conn = getSQLConn(rand(1,3));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "Call AddMinPctChangeByMonth($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year);";
-
-  print_r($sql);
+  SQLInsertUpdateCall("writePctDatatoSQL: ",$sql,3, 1, 1, 0, "NewConfig", 90);
+  /*print_r($sql);
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
-  logAction("writePctDatatoSQL: ".$sql, 'BuyCoin', 0);
+  logAction("writePctDatatoSQL: ".$sql, 'BuyCoin', 0);*/
 }
 
 function getPctChangeFromHistory(){
-  $conn = getHistorySQL(rand(1,4));
+  $tempAry = [];
+  /*$conn = getHistorySQL(rand(1,4));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "SELECT `CoinID`,min(`Hr1Pct`) as Hr1Pct, min(`Hr24Pct`) as Hr24Pct, min(`D7Pct`) as D7Pct, Month(`PriceDate`) as Month,Year(`PriceDate`) as Year  FROM `PriceHistory`
-WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
-AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-and `Hr1Pct` > -20 and `Hr1Pct` < 20
-and `Hr24Pct` > -30 and `Hr24Pct` < 30
-and `D7Pct` > -50 and `D7Pct` < 50
-group by `CoinID`";
-  $result = $conn->query($sql);
+          WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+          AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+          and `Hr1Pct` > -20 and `Hr1Pct` < 20
+          and `Hr24Pct` > -30 and `Hr24Pct` < 30
+          and `D7Pct` > -50 and `D7Pct` < 50
+          group by `CoinID`";
+  $tempAry = mySQLSelect("getPctChangeFromHistory: ",$sql,3,1,1,1,"NewConfig",90);
+  /*$result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
   //mysqli_fetch_assoc($result);
   echo "<BR>$sql";
   while ($row = mysqli_fetch_assoc($result)){
       $tempAry[] = Array($row['CoinID'],$row['Hr1Pct'],$row['Hr24Pct'],$row['D7Pct'],$row['Month'],$row['Year']);
   }
-  $conn->close();
+  $conn->close();*/
   return $tempAry;
 }
 
 function writeAvgPctDatatoSQL($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year){
-  $conn = getSQLConn(rand(1,3));
+  /*$conn = getSQLConn(rand(1,3));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "Call AddAvgMinPctChangeByMonth($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year);";
-
-  print_r($sql);
+  SQLInsertUpdateCall("writeAvgPctDatatoSQL: ",$sql,3, 1, 1, 0, "NewConfig", 90);
+  /*print_r($sql);
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
-  logAction("writeAvgPctDatatoSQL: ".$sql, 'BuyCoin', 0);
+  logAction("writeAvgPctDatatoSQL: ".$sql, 'BuyCoin', 0);*/
 }
 
 function getAvgPctChangeFromHistory(){
-  $conn = getHistorySQL(rand(1,4));
+  $tempAry = [];
+  /*$conn = getHistorySQL(rand(1,4));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "SELECT `CoinID`,avg(`Hr1Pct`) as Hr1Pct, avg(`Hr24Pct`) as Hr24Pct, avg(`D7Pct`) as D7Pct, Month(`PriceDate`) as Month,Year(`PriceDate`) as Year  FROM `PriceHistory`
-WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
-AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-and `Hr1Pct` > -20 and `Hr1Pct` < 0
-and `Hr24Pct` > -30 and `Hr24Pct` < 0
-and `D7Pct` > -50 and `D7Pct` < 0
-group by `CoinID`";
-  $result = $conn->query($sql);
+            WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+            AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+            and `Hr1Pct` > -20 and `Hr1Pct` < 0
+            and `Hr24Pct` > -30 and `Hr24Pct` < 0
+            and `D7Pct` > -50 and `D7Pct` < 0
+            group by `CoinID`";
+  $tempAry = mySQLSelect("getAvgPctChangeFromHistory: ",$sql,3,1,1,1,"NewConfig",90);
+  /*$result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
   //mysqli_fetch_assoc($result);
   echo "<BR>$sql";
   while ($row = mysqli_fetch_assoc($result)){
       $tempAry[] = Array($row['CoinID'],$row['Hr1Pct'],$row['Hr24Pct'],$row['D7Pct'],$row['Month'],$row['Year']);
   }
-  $conn->close();
+  $conn->close();*/
   return $tempAry;
 }
 
 function writeMinPriceDatatoSQL($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year){
-  $conn = getSQLConn(rand(1,3));
+  /*$conn = getSQLConn(rand(1,3));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "Call AddMinPriceChangeByMonth($coinID, $hr1Price, $hr24Price, $d7Price, $month, $year);";
-
-  print_r($sql);
+  SQLInsertUpdateCall("writeMinPriceDatatoSQL: ",$sql,3, 1, 1, 0, "NewConfig", 90);
+  /*print_r($sql);
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
   $conn->close();
-  logAction("writeAvgPctDatatoSQL: ".$sql, 'BuyCoin', 0);
+  logAction("writeAvgPctDatatoSQL: ".$sql, 'BuyCoin', 0);*/
 }
 
 function getMinPriceChangeFromHistory(){
-  $conn = getHistorySQL(rand(1,4));
+  $tempAry = [];
+  /*$conn = getHistorySQL(rand(1,4));
   // Check connection
   if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
-  }
+  }*/
   $sql = "SELECT `CoinID`,`Hr1Pct` as Hr1Pct, `Hr24Pct` as Hr24Pct, `D7Pct` as D7Pct, Month(`PriceDate`) as Month,Year(`PriceDate`) as Year, Min(`Price`) as Price FROM `PriceHistory`
-WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
-AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
-and `Price` > 0
-and `Price` < 999999
-and `Hr1Pct` <> 0
-and `Hr24Pct` <> 0
-and `D7Pct` <> 0
-group by `CoinID`";
-  $result = $conn->query($sql);
+            WHERE YEAR(`PriceDate`) = YEAR(CURRENT_DATE - INTERVAL 1 MONTH)
+            AND MONTH(`PriceDate`) = MONTH(CURRENT_DATE - INTERVAL 1 MONTH)
+            and `Price` > 0
+            and `Price` < 999999
+            and `Hr1Pct` <> 0
+            and `Hr24Pct` <> 0
+            and `D7Pct` <> 0
+            group by `CoinID`";
+  $tempAry = mySQLSelect("getMinPriceChangeFromHistory: ",$sql,3,1,1,1,"NewConfig",90);
+  /*$result = $conn->query($sql);
   //$result = mysqli_query($link4, $query);
   //mysqli_fetch_assoc($result);
   echo "<BR>$sql";
   while ($row = mysqli_fetch_assoc($result)){
       $tempAry[] = Array($row['CoinID'],$row['Hr1Pct'],$row['Hr24Pct'],$row['D7Pct'],$row['Month'],$row['Year'],$row['Price']);
   }
-  $conn->close();
+  $conn->close();*/
   return $tempAry;
 }
 
